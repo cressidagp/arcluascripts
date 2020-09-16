d6d2c05bdae04dcbd054c289882d189faf6b444d
gcpigni <gcpigni@gmail.com>
2020-09-16 14:22:30 -0300

    Added Elders script.

---
+++ EasternKingdoms/Elders.lua
@@ -0,0 +1,33 @@
+local SECRET = 666;
+
+ELDERS = {}
+
+function ELDERS.OnHelloOnSelect( unit, event, plr, id, intid, code )
+		if( event == 1 )
+		then
+				unit:GossipCreateMenu( 13441, plr, 0 );
+				unit:GossipMenuAddItem( 0, "I would like to whisper my secret code to you to receive Tyrael's Hilt.", 2, 1, "", 0 );
+				unit:GossipSendMenu( plr );
+
+		elseif( event == 2 )
+		then
+				if( intid == 2 )
+				then
+						if( code == ""..SECRET.."" )
+						then
+								plr:AddItem( 39656, 1 )
+								plr:GossipComplete();
+						else
+								plr:GossipComplete();
+								unit:SendChatMessage( 12, 0, "You're secret code is not a valid one..." );
+						end
+				end
+		end
+end
+
+-- Stormwind: Ian Drake:
+RegisterUnitGossipEvent( 29093, 1, ELDERS.OnHelloOnSelect );
+RegisterUnitGossipEvent( 29093, 2, ELDERS.OnHelloOnSelect );
+-- Undercity: Edward Cairn:
+RegisterUnitGossipEvent( 29095, 1, ELDERS.OnHelloOnSelect );
+RegisterUnitGossipEvent( 29095, 2, ELDERS.OnHelloOnSelect );
