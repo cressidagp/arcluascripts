f61b6d17bc9dd52195366b8519a5b09fdc1017ec
gcpigni <gcpigni@gmail.com>
2020-09-17 14:08:12 -0300

    Added Feelweaver Scornn out of combat script since this event its bugged in ai_agents.

---
+++ Kalimdor/Durotar/Felweaver_Scornn.lua
@@ -0,0 +1,18 @@
+FELWEAVER_SCORNN = {}
+
+function FELWEAVER_SCORNN.OoCCastBuff( unit, event )
+
+    if( event == 18 )
+    then
+        unit:RegisterAIUpdateEvent( 1000 );
+    else
+        if( unit:IsInCombat() == false and unit:HasAura( 20798 ) == false )
+        then
+            unit:FullCastSpell( 20798 ); -- Demon Skin
+            unit:ModifyAIUpdateEvent( 1800000 );
+        end
+    end
+end
+
+RegisterUnitEvent( 5822, 18, FELWEAVER_SCORNN.OoCCastBuff );
+RegisterUnitEvent( 5822, 21, FELWEAVER_SCORNN.OoCCastBuff );
