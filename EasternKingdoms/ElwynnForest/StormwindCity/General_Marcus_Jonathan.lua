1370ff0b43238c815180054bbb615e96f68bd847
gcpigni <gcpigni@gmail.com>
2020-09-22 01:31:41 -0300

    Added a hostile check to Marcus Jonathan.

--- EasternKingdoms/ElwynnForest/StormwindCity/General_Marcus_Jonathan.lua
+++ EasternKingdoms/ElwynnForest/StormwindCity/General_Marcus_Jonathan.lua
@@ -2,12 +2,12 @@ MARCUS_JONATHAN = {}
 
 function MARCUS_JONATHAN.OnEmote( unit, event, plr, eID )
 
-    if( eID == 3 )
+    if( eID == 3 unit:IsHostile( plr ) == false )
     then
 
         unit:SendChatMessage( 12, 7, "Greetings citizen." );
 
-    elseif( eID == 66 )
+    elseif( eID == 66 unit:IsHostile( plr ) == false )
     then
         unit:Emote( 66, 0 );
     end
