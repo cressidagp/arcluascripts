94477d576fd30d010bdca80d988e2cef4190353f
gcpigni <gcpigni@gmail.com>
2020-09-22 19:51:31 -0300

    Added script for Razormane Quilboar (ooc cast).

---
+++ Kalimdor/Durotar/Razormane_Quilboar.lua
@@ -0,0 +1,18 @@
+RAZORMANE_QUILBOAR = {}
+
+function RAZORMANE_QUILBOAR.OoCCastBuff( unit, event )
+
+    if( event == 18 )
+    then
+        unit:RegisterAIUpdateEvent( 3000 );
+    else
+        if( unit:IsInCombat() == false )
+        then
+            unit:FullCastSpell( 5280 ); -- Razor Mane
+            unit:ModifyAIUpdateEvent( 45000 );
+        end
+    end
+end
+
+RegisterUnitEvent( 3111, 18, RAZORMANE_QUILBOAR.OoCCastBuff );
+RegisterUnitEvent( 3111, 21, RAZORMANE_QUILBOAR.OoCCastBuff );
