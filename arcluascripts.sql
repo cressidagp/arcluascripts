1c0b62411a081d544090963e10424ca2d5c5a1d5
gcpigni <gcpigni@gmail.com>
2020-10-12 02:00:42 -0300

    Forget of remove auras from proto.

--- arcluascripts.sql
+++ arcluascripts.sql
@@ -12,8 +12,22 @@ DELETE FROM `ai_agents` WHERE `entry` = 3206 and `spell` = 324; -- Durotar: Vood
 DELETE FROM `ai_agents` WHERE `entry` = 5822 and `spell` = 20798; -- Durotar: Feelweaver Scornn cast "Demon Skin"
 DELETE FROM `ai_agents` WHERE `entry` = 5826 and `spell` = 324; -- Durotar: Geolord Mottle cast "Lighting Shield"
 
+UPDATE `creature_proto` SET `auras` = 0 WHERE `entry` = 474;
+UPDATE `creature_proto` SET `auras` = 0 WHERE `entry` = 476;
+UPDATE `creature_proto` SET `auras` = 0 WHERE `entry` = 881;
+UPDATE `creature_proto` SET `auras` = 0 WHERE `entry` = 3204;
+UPDATE `creature_proto` SET `auras` = 0 WHERE `entry` = 5822;
+
 --
 -- The next npc_monstersay has been ported to lua:
 --
 
 DELETE FROM `npc_monstersay` WHERE `entry` = 466 and `event` = 0; -- Stormwind City: General Marcus Jonathan
+DELETE FROM `npc_monstersay` WHERE `entry` = 3188 and `event` = 0; -- Durotar: Master Gadrin
+
+--
+-- This one will be handled in lua:
+--
+
+DELETE FROM `creature_spawns` WHERE `id` = 135619 and `entry` = 3289; -- Durotar: Minshinas Spirit
+
