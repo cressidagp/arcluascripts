017e659b04aee684ce7e3e463dee40880f806b2b
gcpigni <gcpigni@gmail.com>
2020-10-19 09:56:32 -0300

    Forget to change HasAura in Vile Fin Murloc Oracle script.

--- EasternKingdoms/TirisfallGlades/Vile_Fin_Minor_Oracle.lua
+++ EasternKingdoms/TirisfallGlades/Vile_Fin_Minor_Oracle.lua
@@ -7,7 +7,7 @@ function VILEFIN_MINORORACLE.OoCCastBuff( unit, event )
         local n = math.random( 1, 5 );
         unit:RegisterAIUpdateEvent( n * 1000 );
     else
-        if( unit:IsInCombat() == false and unit:HasAura( 12544 ) == false )
+        if( unit:IsInCombat() == false and unit:HasAura( 324 ) == false )
         then
             unit:FullCastSpell( 324 ); -- Lightning Shield
             unit:ModifyAIUpdateEvent( 600000 );
