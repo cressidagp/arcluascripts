dce45b008e49c86376365603f392d23a9bda34d1
gcpigni <gcpigni@gmail.com>
2020-10-26 09:23:53 -0300

    Minor fix and header for General MJ.

--- EasternKingdoms/ElwynnForest/StormwindCity/General_Marcus_Jonathan.lua
+++ EasternKingdoms/ElwynnForest/StormwindCity/General_Marcus_Jonathan.lua
@@ -1,16 +1,22 @@
-local EMOTE_ONESHOT_WAVE = 3
-local EMOTE_ONESHOT_SALUTE = 66
+--[[  www.ArcEmu.org
+      Stormwind City: General Marcus Jonathan
+      Engine: A.L.E
+      Credits: to Trinity for waypoints and creature texts
+--]]
+
+local EMOTE_ONESHOT_WAVE = 3;
+local EMOTE_ONESHOT_SALUTE = 66;
 
 MARCUS_JONATHAN = {}
 
 function MARCUS_JONATHAN.OnEmote( unit, event, plr, eID )
 
-    if( eID == EMOTE_ONESHOT_WAVE unit:IsHostile( plr ) == false )
+    if( eID == EMOTE_ONESHOT_WAVE and unit:IsHostile( plr ) == false )
     then
 
         unit:SendChatMessage( 12, 7, "Greetings citizen." );
 
-    elseif( eID == EMOTE_ONESHOT_SALUTE unit:IsHostile( plr ) == false )
+    elseif( eID == EMOTE_ONESHOT_SALUTE and unit:IsHostile( plr ) == false )
     then
         unit:Emote( EMOTE_ONESHOT_SALUTE, 0 );
     end
