378676155853489c330a57e4a1beeb9f7d3d584e
gcpigni <gcpigni@gmail.com>
2020-10-26 09:36:22 -0300

    Rename table and header in Morgan The Collector script.

--- EasternKingdoms/ElwynnForest/Morgan_the_Collector.lua
+++ EasternKingdoms/ElwynnForest/Morgan_the_Collector.lua
@@ -1,15 +1,21 @@
+--[[  www.ArcEmu.org
+      Elwynn Forest: Morgan The Collector
+      Engine: A.L.E
+      Credits: nil
+--]]
+
 local SPELL_GOUGE = 1776;
 
-MORGAN_COLLECTOR = {}
+MORGAN_THE_COLLECTOR = {}
 
-function MORGAN_COLLECTOR.Gouge( unit, event )
-    unit:CastSpellOnTarget( SPELL_GOUGE, MORGAN_COLLECTOR.target );
+function MORGAN_THE_COLLECTOR.Gouge( unit, event )
+    unit:CastSpellOnTarget( SPELL_GOUGE, MORGAN_THE_COLLECTOR.target );
 end
 
-function MORGAN_COLLECTOR.OnCombat( unit, event, attacker )
-    MORGAN_COLLECTOR.target = attacker;
-    unit:RegisterEvent( MORGAN_COLLECTOR.Gouge, 2000, 1 );
-    unit:RegisterEvent( MORGAN_COLLECTOR.Gouge, math.random( 12000, 14000 ), 0 );
+function MORGAN_THE_COLLECTOR.OnCombat( unit, event, attacker )
+    MORGAN_THE_COLLECTOR.target = attacker;
+    unit:RegisterEvent( MORGAN_THE_COLLECTOR.Gouge, 2000, 1 );
+    unit:RegisterEvent( MORGAN_THE_COLLECTOR.Gouge, math.random( 12000, 14000 ), 0 );
 end
 
-RegisterUnitEvent( 473, 1, MORGAN_COLLECTOR.OnCombat );
+RegisterUnitEvent( 473, 1, MORGAN_THE_COLLECTOR.OnCombat );
