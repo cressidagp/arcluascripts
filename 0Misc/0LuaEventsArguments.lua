369d88556e2da83725ebdbf36c804196fd59fbdb
cressidagp <gcpigni@gmail.com>
2020-12-02 15:05:52 -0300

    Finished with creature and gameobject arguments documentation.

--- 0Misc/0LuaEventsArguments.lua
+++ 0Misc/0LuaEventsArguments.lua
@@ -1,6 +1,6 @@
 --[[
 
-//Creature Events:
+//Creature Event Arguments:
 
 OnEnterCombat( unit, event, attacker )
 OnLeaveCombat( unit, event, target )
@@ -20,10 +20,42 @@ OnFear( unit, event, targetFeared, spell_id )
 OnFlee( unit, event, fearer )
 OnCallForHelp( unit, event )
 OnLoad( unit, event )
-OnReachWaypoint( unit, event, waypointId, unknown )
+OnReachWaypoint( unit, event, waypointId, foward )
 OnLookTaken( unit, event, player, item )
 OnAIUpdate( unit, event )
 OnEmote( unit, event, player, emoteId )
 OnDamageTaken( unit, event, attacker, damage )
+OnEnterVehicle( unit, event )
+OnExitVehicle( unit, event )
+OnFirstPassengerEntered( unit, event, passanger )
+OnVehicleFull( unit, event )
+OnLastPassengerLeft( unit, event, passanger )
+
+//Gameobjects Events Arguments:
+
+OnCreate( go, event )
+OnSpawn( go, event )
+OnLootTaken( go, event, player, item )
+OnUse( go, event, player )
+OnAIUpdate( go, event )
+OnDespawn( go, event )
+OnDamaged( go, event, damage )
+OnDestroyed( go, event )
+
+//Gossips Events Arguments:
+
+OnTalk()
+OnSelectOption()
+OnEnd()
+
+//Quest Events:
+
+OnAccept()
+OnComplete()
+OnCancel()
+OnGameObjectActivate()
+OnCreatureKill()
+OnExploreArea()
+OnItemPickup()
 
 --]]
