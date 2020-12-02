587154a25adff0074e410f907665b7c25569f4b9
cressidagp <gcpigni@gmail.com>
2020-12-02 14:24:59 -0300

    Started to work on event arguments documentation.

---
+++ 0Misc/0LuaEventsArguments.lua
@@ -0,0 +1,29 @@
+--[[
+
+//Creature Events:
+
+OnEnterCombat( unit, event, attacker )
+OnLeaveCombat( unit, event, target )
+OnTargetDied( unit, event, victim )
+OnDied( unit, event, killer )
+OnTargetParried( unit, event, target )
+OnTargetDodged( unit, event, target )
+OnTargetBlocked( unit, event, target, ammount )
+OnTargetCritHit( unit, event, target, ammount )
+OnParry( unit, event, currentTarget )
+OnDodged( unit, event, currentTarget )
+OnBlocked( unit, event, currentTarget, ammount )
+OnCritHit( unit, event, currentTarget, ammount )
+OnHit( unit, event, currentTarget, ammount )
+OnAssistTargetDied( unit, event, assistTarget )
+OnFear( unit, event, targetFeared, spell_id )
+OnFlee( unit, event, fearer )
+OnCallForHelp( unit, event )
+OnLoad( unit, event )
+OnReachWaypoint( unit, event, waypointId, unknown )
+OnLookTaken( unit, event, player, item )
+OnAIUpdate( unit, event )
+OnEmote( unit, event, player, emoteId )
+OnDamageTaken( unit, event, attacker, damage )
+
+--]]
