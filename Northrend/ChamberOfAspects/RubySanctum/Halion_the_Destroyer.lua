479c94990972808b6cba8154baf8938fab12a121
cressidagp <gcpigni@gmail.com>
2020-12-03 19:53:14 -0300

    Back to work on Halion the Destroyer. Still far far away from this one.

--- Northrend/ChamberOfAspects/RubySanctum/Halion_the_Destroyer.lua
+++ Northrend/ChamberOfAspects/RubySanctum/Halion_the_Destroyer.lua
@@ -3,8 +3,8 @@
 	www.ArcEmu.org
 	The Ruby Sanctum: Halion the Destroyer
 	Engine: A.L.E
-	Credits: Trinity for texts and sound ids.
-
+	Credits: Trinity for texts, sound ids, timers and spell ids.
+			
 	Developer notes: in time i will change this to paroxysm modular way to save some resources.
 
 --]]
@@ -23,10 +23,10 @@ local SOUND = {
 
 local CHAT = {
 [ 1 ] = "Meddlesome insects! You are too late. The Ruby Sanctum is lost!";  -- Intro
-[ 3 ] = "The heavens burn!";  -- OnMeterStrike
-[ 4 ] = "You will find only suffering within the realm of twilight! Enter if you dare!";  -- OnPhaseTwo
-[ 6 ] = "Another hero falls.";  -- OnTargetDied (shoud be "hero", but "" caused problems)
-[ 7 ] = "Not good enough....";
+[ 2 ] = "The heavens burn!";  -- OnMeterStrike
+[ 3 ] = "You will find only suffering within the realm of twilight! Enter if you dare!";  -- OnPhaseTwo
+[ 4 ] = "Another 'hero' falls.";  -- OnTargetDied (shoud be "hero", but "" caused problems)
+[ 5 ] = "Not good enough...."; -- OnBerserk
 };
 
 local TEXT = {
@@ -36,44 +36,90 @@ local TEXT = {
 };
 
 -- Spells:
-SPELL_FLAME_BREATH 		= 74525;
-SPELL_CLEAVE 			= 74524;
-SPELL_METEOR_STRIKE 	= 74637; -- aoe, need a dummy
-SPELL_TAIL_LASH 		= 74531;
+local SPELL_FLAME_BREATH		= 74525;
+local SPELL_CLEAVE				= 74524;
+local SPELL_METEOR_STRIKE		= 74637; -- aoe, need a dummy
+local SPELL_TAIL_LASH			= 74531;
+local SPELL_TWILIGHT_PRECISION  = 78243; -- not aura displayed... not working?
+
+-- For 3.3.5a
+local UNIT_FIELD_FLAGS			= 0x0006 + 0x0035;
+local UNIT_FLAG_NOT_SELECTABLE	= 0x02000000;
+local GAMEOBJECT_BYTES_1		= 0x0006 + 0x000B;
 
 local self = getfenv( 1 );
 
-function OnSpawn( unit, event )
+function OnSpawn( unit )
 
     local diff = unit:GetDungeonDifficulty();
-	
+
     unit:SetMaxHealth( BOSS_HP [ diff + 1 ] );
-	
+
     unit:SetHealth( BOSS_HP [ diff + 1 ] );
 
 end
 
-function OnCombat( unit, event )
+function OnCombat( unit )
 
-		self[ tostring( unit )] = {
+	self[ tostring( unit )] = {
 
-		phase = 1,
-		flamebreath = math.random( 5, 15 ),
-		cleave = math.random( 6, 10 ),
-		tail = math.random( 7, 12 ),
-		fierycombustion = math.random( 15, 18 ),
-		meteorstrike = 18
+	phase = 1,
+	firewallTime = 5,
+	firewallInClosed = false,
+	flamebreath = math.random( 5, 15 ),
+	cleave = math.random( 6, 10 ),
+	tail = math.random( 7, 12 ),
+	fierycombustion = math.random( 15, 18 ),
+	meteorstrike = 18
 
-		};
+	};
+
+	--[[ Developer notes: we dont need to send the chat here since
+    our monstersay table will do the job, instance collision checked. ]]
 
     unit:PlaySoundToSet( SOUND[ 2 ] );
-
-    --[[ Developer notes: we dont need to send the chat here since
-    our monstersay table will do the job, instance collision checked. ]]
+	
+	unit:AddAura( SPELL_TWILIGHT_PRECISION, 0 );
+	unit:RegisterAIUpdateEvent( 1000 );
 
 end
 
-function OnLeaveCombat( unit, event )
+function OnDamageTaken( unit, event, attacker, damage )
+
+	local vars = self[ tostring( unit ) ];
+	
+	local hp = unit:GetHealth();
+	local pct = unit:GetHealthPct();
+
+    if( damage >= hp and vars.phase ~= 3 )
+	then
+        damage = hp - 1;
+		
+	end
+	
+	-- TODO: implement HealthBelowPctDamaged( %, damage )
+	
+	if( pct < 75 and vars.phase == 1 )
+	then
+		unit:PlaySoundToSet( SOUND[ 4 ] );
+		unit:SendChatMessage( 14, 0, CHAT[ 4 - 1 ] );
+		vars.phase = 2;
+		
+	end
+	
+	if( vars.phase == 3 )
+	then
+		if( unit:GetPhase() ~= attacker:GetPhase() )
+		then 
+			return; 
+		end
+		
+		local controller = unit:GetCreatureNearestCoords( 3156.04, 533.266, 72.9721, 40146 );
+		controller:SetHealth( controller:GetHealth() - damage );
+	end
+end
+
+function OnLeaveCombat( unit )
 
 	-- destroy table with variables to recycle resources
 
@@ -87,26 +133,49 @@ function OnLeaveCombat( unit, event )
 
 end
 
-function OnTargetDied( unit, event )
+function OnTargetDied( unit, _, victim )
 
-    unit:PlaySoundToSet( SOUND[ 6 ] );
-    unit:SendChatMessage( 14, 0, CHAT[ 6 ] );
-
+	if( victim:IsPlayer() == true )
+	then
+    	unit:PlaySoundToSet( SOUND[ 6 ] );
+    	unit:SendChatMessage( 14, 0, CHAT[ 6 - 2 ] );
+	end
 end
 
-function OnDeath( unit, event )
+function OnDeath( unit )
+
+	-- destroy table with variables to recycle resources
+
+	self[ tostring( unit ) ] = nil;
+
+	--[[ Developer notes: we dont need to send the chat here since our
+	monstersay table will do the job, instance collision checked. ]]
 
     unit:PlaySoundToSet( SOUND[ 5 ] );
-
-    --[[ Developer notes: we dont need to send the chat here since our
-    monstersay table will do the job, instance collision checked. ]]
-
+	
+	local x = unit:GetX();
+	local y = unit:GetY();
+	local z = unit:GetZ();
+	
+	local twlighthalion = unit:GetClosestCreature( x, y, z, 40142 );
+	
+	if( twlighthalion:IsAlive() == true )
+	then
+		twlighthalion:Kill();
+	end
+	
+	local controller = unit:GetClosestCreature( x, y, z, 40146 );
+	
+	if( controller:IsAlive() == true )
+	then
+		controller:Kill();
+	end
 end
 
-function OnAIUpdate( unit, event )
+function OnAIUpdate( unit )
 
 	if( unit:IsCasting() == true ) then return; end
-	
+
 	if( unit:GetNextTarget() == nil ) then
 		unit:WipeThreatList()
 		return;
@@ -114,11 +183,20 @@ function OnAIUpdate( unit, event )
 
 	local vars = self[ tostring( unit ) ];
 
+	vars.firewall = vars.firewall - 1;
 	vars.flamebreath = vars.flamebreath - 1;
 	vars.cleave = vars.cleave - 1;
 	vars.tail = vars.tail - 1;
 
-	if( vars.flamebreath <= 0 )
+	if( vars.firewallTime <= 0 and vars.firewallIsClosed == false )
+	then
+		local flamering = unit:GetGameObjectNearestCoords( 3154.99, 535.64, 72.89, 203007 );
+		flamering:SetByte( GAMEOBJECT_BYTES_1, 0, 0 );
+		vars.firewallTime = 0;
+		vars.firewallIsClosed = true;
+
+
+	elseif( vars.flamebreath <= 0 )
     then
 		unit:FullCastSpell( SPELL_FLAME_BREATH );
 		unit:SendChatMessage( 12, 0, "debug: flame breath" );
@@ -136,17 +214,32 @@ function OnAIUpdate( unit, event )
 		unit:SendChatMessage( 12, 0, "debug: tail lash" );
 		vars.tail = math.random( 7, 12 )
 	end
-end
-
-function HalionControllerOnSpawn( unit, event )
-
-end
+end 
 
 RegisterUnitEvent( 39863, 18, OnSpawn );
 RegisterUnitEvent( 39863, 1 , OnCombat );
+RegisterUnitEvent( 39863, 23, OnDamageTaken );
 RegisterUnitEvent( 39863, 2 , OnLeaveCombat );
 RegisterUnitEvent( 39863, 3 , OnTargetDied );
 RegisterUnitEvent( 39863, 4 , OnDeath );
 RegisterUnitEvent( 39863, 21, OnAIUpdate );
+
+--[[
+			Twilight Halion AI
+--]]
+
+function TwilightHalionOnSpawn( unit )
+
+end
+
+RegisterUnitEvent( 40142, 18 , TwilightHalionOnSpawn );
+
+--[[
+			Halion Controller AI
+--]]
+
+function HalionControllerOnSpawn( unit, event )
+
+end
 
 RegisterUnitEvent( 40146, 18 , HalionControllerOnSpawn );
