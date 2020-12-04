0b263173f05e3fb460e0584f6df76e66d78e0a05
cressidagp <gcpigni@gmail.com>
2020-12-04 18:01:54 -0300

    Added a file with Entry Registers. Just documentation, saves time having it close to your script folder while navigating in the code editor.

---
+++ 0Misc/0Entry_Registers.lua
@@ -0,0 +1,23 @@
+--[[
+
+//Entry Registers:
+
+RegisterUnitEvent( creature_entry, creature_event, function_name );
+
+RegisterGameObjectEvent( gameobject_entry, gameobject_event, function_name );
+
+RegisterUnitGossipEvent( creature_entry, gossip_event, function_name );
+
+RegisterGOGossipEvent( gameobject_entry, gossip_event, function_name );
+
+RegisterItemGossipEvent( item_entry, gossip_event, function_name );
+
+RegisterQuestEvent( quest_id, quest_event, function_name );
+
+RegisterDummySpell( spell_id, function_name );
+
+RegisterInstanceEvent( instance_map, instance_event, function_name );
+
+RegisterServerHook( server_hook_event, function_name );
+
+--]]
