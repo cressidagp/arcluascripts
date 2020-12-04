7dbc679cf1ddb07268cfdb1f1a729fd888228b16
cressidagp <gcpigni@gmail.com>
2020-12-04 18:54:38 -0300

    Insignificant cleanup.

--- EasternKingdoms/ElwynnForest/Morgan_the_Collector.lua
+++ EasternKingdoms/ElwynnForest/Morgan_the_Collector.lua
@@ -1,4 +1,4 @@
---[[ 
+--[[
 	ArcLuaScripts for ArcEmu
 	www.ArcEmu.org
 	Elwynn Forest: Morgan The Collector
@@ -6,24 +6,24 @@
 	Credits: nil
 --]]
 
-local SPELL_GOUGE = 1776;
-
 MORGAN_THE_COLLECTOR = {}
 
 function MORGAN_THE_COLLECTOR.Gouge( unit )
 
-    unit:CastSpellOnTarget( SPELL_GOUGE, MORGAN_THE_COLLECTOR.target );
-	
+	-- Cast: "Gouge"
+
+    unit:CastSpellOnTarget( 1776, MORGAN_THE_COLLECTOR.target );
+
 end
 
 function MORGAN_THE_COLLECTOR.OnCombat( unit, _, attacker )
 
     MORGAN_THE_COLLECTOR.target = attacker;
-	
+
     unit:RegisterEvent( MORGAN_THE_COLLECTOR.Gouge, 2000, 1 );
-	
+
     unit:RegisterEvent( MORGAN_THE_COLLECTOR.Gouge, math.random( 12000, 14000 ), 0 );
-	
+
 end
 
 RegisterUnitEvent( 473, 1, MORGAN_THE_COLLECTOR.OnCombat );
