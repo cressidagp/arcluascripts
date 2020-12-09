184e629880c2cff4e3c0d1fee5a1f9e5ec90285f
cressidagp <gcpigni@gmail.com>
2020-12-09 07:19:51 -0300

    Added Sputtervalve gossip script.

---
+++ Kalimdor/Barrens/Sputtervalve.lua
@@ -0,0 +1,35 @@
+--[[
+	ArcLuaScripts for ArcEmu
+	www.ArcEmu.org
+	The Barrens: Sputtervalve (Ratchet)
+	Engine: A.L.E
+
+	Credits:
+
+	*) Trinity for texts.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
+
+--]]
+
+SPUTTERVALVE = {}
+
+function SPUTTERVALVE.OnHelloOnSelect( unit, event, plr, id, selection, code )
+
+	if( event == 1 )
+	then
+		unit:GossipCreateMenu( 519, plr, 0 );
+		unit:GossipMenuAddItem( 0, "How can I help?", 1, 0 );
+		unit:GossipSendMenu( plr );
+	else
+		if( selection == 1 )
+		then
+			unit:GossipCreateMenu( 518, plr, 0 );
+			unit:GossipSendMenu( plr );
+		end
+	end
+end
+
+RegisterUnitGossipEvent( 3442, 1, SPUTTERVALVE.OnHelloOnSelect );
+RegisterUnitGossipEvent( 3442, 2, SPUTTERVALVE.OnHelloOnSelect );
