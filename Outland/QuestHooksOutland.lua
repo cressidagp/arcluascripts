10f41deeb097c8f2f9b480cd155dda3cd031529d
cressidagp <gcpigni@gmail.com>
2020-12-15 15:57:33 -0300

    Added better credits.

--- Outland/QuestHooksOutland.lua
+++ Outland/QuestHooksOutland.lua
@@ -1,3 +1,20 @@
+--[[
+	ArcLuaScripts for ArcEmu
+	www.ArcEmu.org
+	Outland Quest Hooks
+	Engine: A.L.E
+
+  Credits:
+
+  *) Trinity for gossips, texts, sound ids, timers, spell ids, move coords and some inspiration.
+  *) Hypersniper for his lua guides and some job in the lua engine.
+  *) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+  *) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
+
+	Developer notes: if this grows to big i will split on zones.
+
+--]]
+
 QUESTS_HOOK_OUTLAND = {}
 
 function QUESTS_HOOK_OUTLAND.OnComplete( event, plr, questID, questEnder )
@@ -7,7 +24,7 @@ function QUESTS_HOOK_OUTLAND.OnComplete( event, plr, questID, questEnder )
     if( questID == 10254 )
     then
       questEnder:SendChatMessage( 12, 0, "Welcome to Honor Hold, "..plr:GetName()..".  It's good to have you." );
-      -- questEnder:CastSpellOnTarget( 6245 );
+      --questEnder:CastSpellOnTarget( 6245, plr );
       questEnder:Emote( 66, 0 );
     end
 end
