b847e3bf322deb914c3121fd01606ce36c385a17
cressidagp <gcpigni@gmail.com>
2020-12-16 13:23:38 -0300

    Found a way to display quest in gossips.

--- Outland/HellfirePeninsula/Danath_Trollbane.lua
+++ Outland/HellfirePeninsula/Danath_Trollbane.lua
@@ -20,35 +20,35 @@ function DANATH_TROLLBANE.OnHello( unit, _, plr )
 	if( plr:HasFinishedQuest( 10254 ) == true )
 	then
 		unit:GossipCreateMenu( 9939, plr, 0 );
-    unit:GossipMenuAddItem( 0, "Tell me of the Sons of Lothar.", 1, 0 );
-    unit:GossipMenuAddItem( 0, "Tell me of the Hellfire orcs.", 2, 0 );
-    unit:GossipMenuAddItem( 0, "Tell me of your homeland.", 3, 0 );
+		unit:GossipMenuAddItem( 0, "Tell me of the Sons of Lothar.", 1, 0 );
+		unit:GossipMenuAddItem( 0, "Tell me of the Hellfire orcs.", 2, 0 );
+		unit:GossipMenuAddItem( 0, "Tell me of your homeland.", 3, 0 );
 
 	else
-			unit:GossipCreateMenu( 9939, plr, 0 );
-		end
+		unit:GossipCreateMenu( 9939, plr, 0 );
+	end
+		unit:GossipAddQuests( plr );
 		unit:GossipSendMenu( plr );
 end
 
 function DANATH_TROLLBANE.OnSelect( unit, _, plr, id, selection )
 
-  if( selection == 1 )
-  then
-    unit:GossipCreateMenu( 9932, plr, 0 );
-    unit:GossipMenuAddItem( 0, "<more>", 4, 0 );
+	if( selection == 1 )
+	then
+		unit:GossipCreateMenu( 9932, plr, 0 );
+		unit:GossipMenuAddItem( 0, "<more>", 4, 0 );
 
-  elseif( selection == 2 )
-  then
-    unit:GossipCreateMenu( 9941, plr, 0 );
+	elseif( selection == 2 )
+	then
+		unit:GossipCreateMenu( 9941, plr, 0 );
 
-  elseif( selection == 3 )
-  then
-    unit:GossipCreateMenu( 8772, plr, 0 );
-
-  else
-    unit:GossipCreateMenu( 9933, plr, 0 );
-  end
-  unit:GossipSendMenu( plr );
+	elseif( selection == 3 )
+	then
+		unit:GossipCreateMenu( 8772, plr, 0 );
+	else
+		unit:GossipCreateMenu( 9933, plr, 0 );
+	end
+	unit:GossipSendMenu( plr );
 end
 
 RegisterUnitGossipEvent( 16819, 1, DANATH_TROLLBANE.OnHello );
