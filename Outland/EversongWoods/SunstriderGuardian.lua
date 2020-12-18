c3d0c9b897a9d5cfff37a45905ff2c58a52623bc
cressidagp <gcpigni@gmail.com>
2020-12-18 16:46:32 -0300

    Arguments cleanup and typos in Sunstrider Guardian script.

--- Outland/EversongWoods/SunstriderGuardian.lua
+++ Outland/EversongWoods/SunstriderGuardian.lua
@@ -19,35 +19,35 @@
 
 	esMX:
 
-  [ 1 ] = "Muévete, "..plr:GetPlayerClass().."!";
-  [ 2 ] = "Fuera contigo, "..plr:GetName().."!";
+  [ 1 ] = "¡Muévete, "..plr:GetPlayerClass().."!";
+  [ 2 ] = "¡Fuera contigo, "..plr:GetName().."!";
   [ 3 ] = "¡No me presione ciudadano!";
 
 --]]
 
 SUNSTRIDER_GUARDIAN = {}
 
-function SUNSTRIDER_GUARDIAN.OnEmote( unit, event, plr, emoteId )
-  if( emoteId == 14 ) -- rude
-  then
-    unit:EventChat( 12, 0, "Do not push it citizen!", 1000 );
-    unit:Emote( 1, 0 );
+function SUNSTRIDER_GUARDIAN.OnEmote( unit, _, plr, emoteId )
+	if( emoteId == 14 ) -- rude
+	then
+		unit:EventChat( 12, 0, "Do not push it citizen!", 1000 );
+		unit:Emote( 1, 0 );
 
-  elseif( emoteId == 66 ) -- salute
-  then
-    local chance = math.random( 1, 3 );
-    if( chance == 1 )
-    then
-      local i = math.random( 1, 2 );
-      if( i == 1 )
-      then
-        unit:EventChat( 12, 0, "Move along, "..plr:GetPlayerClass().."!", 1000 );
-      else
-        unit:EventChat( 12, 0, "Off with you, "..plr:GetName().."!", 1000 );
-      end
-      unit:Emote( 66, 0 );
-    end
-  end
+	elseif( emoteId == 66 ) -- salute
+	then
+		local chance = math.random( 1, 3 );
+		if( chance == 1 )
+		then
+			local i = math.random( 1, 2 );
+			if( i == 1 )
+			then
+				unit:EventChat( 12, 0, "Move along, "..plr:GetPlayerClass().."!", 1000 );
+			else
+				unit:EventChat( 12, 0, "Off with you, "..plr:GetName().."!", 1000 );
+			end
+			unit:Emote( 66, 0 );
+		end
+	end
 end
 
 RegisterUnitEvent( 15371, 22, SUNSTRIDER_GUARDIAN.OnEmote );
