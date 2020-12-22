e5faa8093ca6c564f6f878c3ced559be5b02182b
cressidagp <gcpigni@gmail.com>
2020-12-22 15:12:56 -0300

    Added proper credits and changed codestyle in HooksMainScript

--- scripts/ServerHooks/HooksMainScript.lua
+++ scripts/ServerHooks/HooksMainScript.lua
@@ -3,11 +3,16 @@
 	www.ArcEmu.org
 	Hooks Main script
 	Engine: A.L.E
-	Credits: nil
+
+	Credits:
+
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
 	
 	Features:
 	
-	*) Add the gm items to new characters.
+	*) Add gm items to new characters.
 	*) Add "Heroic Presence" aura for Draneis players.
 	*) Cast "Blood Presence" and add "Undying Resolve" for Death Knights.
 
@@ -29,25 +34,25 @@ function HOOKS.Consolidated( event, plr )
 			end
 		end
 
-    elseif( event == 4 ) -- OnEnterWorld
-    then
-        if( plr:GetPlayerRace() == 11 )
-        then
-            local class = plr:GetPlayerClass();
-            if( class == "Priest" or class == "Shaman" or class == "Mage" )
-            then
-                plr:AddAura( 28878, 0 ); -- Heroic Presence
-            else
-                plr:AddAura( 6562, 0 ); -- Heroic Presence
-            end
-        end
+	elseif( event == 4 ) -- OnEnterWorld
+	then
+		if( plr:GetPlayerRace() == 11 )
+		then
+			local class = plr:GetPlayerClass();
+			if( class == "Priest" or class == "Shaman" or class == "Mage" )
+			then
+				plr:AddAura( 28878, 0 ); -- Heroic Presence
+			else
+				plr:AddAura( 6562, 0 ); -- Heroic Presence
+			end
+		end
 
-        if( plr:GetPlayerClass() == "Death Knight" )
-        then
-            plr:CastSpell( 48266 ); -- Blood Presence
-            plr:AddAura( 51915, 0 ); -- Undying Resolve
-        end
-    end
+		if( plr:GetPlayerClass() == "Death Knight" )
+		then
+			plr:CastSpell( 48266 ); -- Blood Presence
+			plr:AddAura( 51915, 0 ); -- Undying Resolve
+		end
+	end
 end
 
 RegisterServerHook( 3, HOOKS.Consolidated );
