8dd923b7c58c7462e1ca383fc83fdcc5884e97c6
cressidagp <gcpigni@gmail.com>
2020-12-22 23:27:15 -0300

    Added a hook to remove Undying Resolve when leaving Ebon Hold.

---
+++ scripts/ServerHooks/HooksOnZone.lua
@@ -0,0 +1,25 @@
+--[[
+	ArcLuaScripts for ArcEmu
+	www.ArcEmu.org
+	Hooks Zone
+	Engine: A.L.E
+	
+	Credits:
+
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
+
+--]]
+
+HOOKS_ZONE = {}
+
+function HOOKS_ZONE.OnEnterOrLeave( _, plr, _, OldZoneId )
+
+	if( OldZoneId == 4298 ) -- Ebon Hold
+	then
+		plr:RemoveAura( 51915 ); -- Undying Resolve
+	end
+end
+
+RegisterServerHook( 15, HOOKS_ZONE.OnEnterOrLeave );
