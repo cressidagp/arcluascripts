5f2999d21cc6d81f68bbcd9a883d1f3545b68607
cressidagp <gcpigni@gmail.com>
2021-01-12 16:50:26 -0300

    Documented a little.

--- scripts/ServerHooks/HooksOnZone.lua
+++ scripts/ServerHooks/HooksOnZone.lua
@@ -3,12 +3,15 @@
 	www.ArcEmu.org
 	Hooks Zone
 	Engine: A.L.E
-	
+
 	Credits:
 
 	*) Hypersniper for his lua guides and some job in the lua engine.
 	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
 	*) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
+
+local ZONE_EBON_BLADE	= 4298;
+local SPELL_UNDYING_RESOLVE	= 51915;
 
 --]]
 
