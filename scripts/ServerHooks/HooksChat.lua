64904e49e954ebe24a0ed189308710c35a3c2bd9
cressidagp <gcpigni@gmail.com>
2021-01-12 16:06:47 -0300

    Rewrited HooksChat to be more efficient, also remove not needed arguments.

--- scripts/ServerHooks/HooksChat.lua
+++ scripts/ServerHooks/HooksChat.lua
@@ -3,12 +3,7 @@
 	www.ArcEmu.org
 	Hooks Chat
 	Engine: A.L.E
-
-	Credits:
-
-	*) Hypersniper for his lua guides and some job in the lua engine.
-	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
-	*) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
+	Credits: nil
 
 --]]
 
@@ -16,7 +11,7 @@ local COMMANDS = { "help", "gprint", "removeauras", "getphase", "jail", "entry" 
 
 HOOKS_CHAT = {}
 
-function HOOKS_CHAT.AllCommands( event, plr, msg, type, lang, misc )
+function HOOKS_CHAT.AllCommands( event, plr, msg )
 
 	if( plr:IsGm() == true )
 	then
@@ -26,65 +21,49 @@ function HOOKS_CHAT.AllCommands( event, plr, msg, type, lang, misc )
 			do
 				plr:SendBroadcastMessage( ""..COMMANDS[ i ].."" );
 			end
-	
+
 		elseif( msg == "#gprint" )
 		then
 			for n in pairs( _G )
-			do 
+			do
 				print( n );
 				plr:SendBroadcastMessage( ""..n.."" );
 			end
-		
+
 			plr:SendBroadcastMessage( "Lua global table has been printed." );
-		
-		--.debug rangecheck
-		--elseif( msg == "#distance" )
-		--then
-			--local target = plr:GetSelection();
-			--plr:SendBroadcastMessage( "Distance: "..plr:GetDistance( target ).."" );
-			--plr:SendBroadcastMessage( "Use .debug dist to get distance in yards." );
-		
-		elseif( msg == "#removeauras" )
-		then
-			local target = plr:GetSelection();
-			if( target ~= nil )
-			then
-				target:RemoveAllAuras();
-				plr:SendBroadcastMessage( "All auras has been removed from the target." );
-			else
-				plr:SendBroadcastMessage( "You dont have a target." );
-			end
 			
-		elseif( msg == "#getphase" )
-		then
+		else
 			local target = plr:GetSelection();
-			if( target ~= nil )
+			if( target == nil )
 			then
-				plr:SendBroadcastMessage( "Phase: "..target:GetPhase().."" );
-			else
 				plr:SendBroadcastMessage( "You dont have a target." );
-			end
-		
-		elseif( msg == "#jail" )
-		then
-			local target = plr:GetSelection();
-			if( target ~= nil )
-			then
-				target:Teleport( 0, -8667.677, 624.130, 95.69, 2.2 );
-				target:SendBroadcastMessage( "You are in jail." );
-				plr:SendBroadcastMessage( "Player has been send to jail." );
+				
 			else
-				plr:SendBroadcastMessage( "You dont have a target." );
-			end
+				if( msg == "#removeauras" )
+				then
+					target:RemoveAllAuras();
+				
+				elseif( msg == "#getphase" )
+				then
+					plr:SendBroadcastMessage( "Phase: "..target:GetPhase().."" );
+				
+				elseif( msg == "#jail" )
+				then
+					target:Teleport( 0, -8667.677, 624.130, 95.69, 2.2 );
+					target:SendBroadcastMessage( "You are in jail." );
+					plr:SendBroadcastMessage( "Player has been send to jail." );					
 			
-		elseif( msg == "#entry" )
-		then
-			local target = plr:GetSelection();
-			if( target ~= nil )
-			then
-				plr:SendBroadcastMessage( "Entry: "..target:GetEntry().."" );
-			else
-				plr:SendBroadcastMessage( "You dont have a target." );
+				elseif( msg == "#entry" )
+				then
+					plr:SendBroadcastMessage( "Entry: "..target:GetEntry().."" );
+				
+				--[[ debug rangecheck
+				elseif( msg == "#distance" )
+				then 
+					plr:SendBroadcastMessage( "Distance: "..plr:GetDistance( target ).."" );
+					plr:SendBroadcastMessage( "Use .debug dist to get distance in yards." );
+				--]]
+				end
 			end
 		end
 	end
