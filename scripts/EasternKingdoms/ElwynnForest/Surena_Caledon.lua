7c3bc1ac0bc75f33ec86aa9f8c17405e73185bac
cressidagp <gcpigni@gmail.com>
2021-01-15 18:16:35 -0300

    Minor changes at Surena Caledon script.

--- scripts/EasternKingdoms/ElwynnForest/Surena_Caledon.lua
+++ scripts/EasternKingdoms/ElwynnForest/Surena_Caledon.lua
@@ -1,39 +1,39 @@
---[[ 
+--[[
 	ArcLuaScripts for ArcEmu
 	www.ArcEmu.org
-	Elwynn Forest: Surena Caledon
 	Engine: A.L.E
-	Credits: nil
+	
+	Zone: Elwynn Forest
+	Creature: Surena Caledon (881)	
+
+	Credits:
+	
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
 --]]
 
 SURENA_CALEDON = {}
 
 function SURENA_CALEDON.OoCCastBuff( unit, event )
 
-    if( event == 18 )
-	
+	-- on ai update
+    if( event == 21 )
     then
-	
-		local n = math.random( 1, 5 );
-		
-        unit:RegisterAIUpdateEvent( n * 1000 );
-		
-    else
-	
         if( unit:IsInCombat() == false and unit:HasAura( 12544 ) == false )
-		
         then
-		
             unit:FullCastSpell( 12544 ); -- Frost Amor
-			
             unit:ModifyAIUpdateEvent( 1800000 );
-			
         end
-		
-    end
 	
+	-- on spawn
+    else
+		local n = math.random( 3, 6 );
+		unit:RegisterAIUpdateEvent( n * 1000 );
+
+    end
 end
 
 RegisterUnitEvent( 881, 18, SURENA_CALEDON.OoCCastBuff );
-
 RegisterUnitEvent( 881, 21, SURENA_CALEDON.OoCCastBuff );
