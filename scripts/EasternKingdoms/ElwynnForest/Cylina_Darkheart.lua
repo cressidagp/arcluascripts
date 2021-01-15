bdf05e6006b95d794b9052f5e08d3b891a6932b7
cressidagp <gcpigni@gmail.com>
2021-01-15 18:30:44 -0300

    Documented Cylina Darkheart script.

--- scripts/EasternKingdoms/ElwynnForest/Cylina_Darkheart.lua
+++ scripts/EasternKingdoms/ElwynnForest/Cylina_Darkheart.lua
@@ -1,13 +1,21 @@
 --[[
-      ArcLuaScripts for ArcEmu
-      www.ArcEmu.org
-      Elwynn Forest: Cylina Darkheart and Minion
-      Engine: A.L.E
-      Credits: nil
+	ArcLuaScripts for ArcEmu
+	www.ArcEmu.org
+	Engine: A.L.E
+	
+	Zone: Elwynn Forest
+	Creature: Cylina Darkheart (6374)	
+	
+	Credits:
+	
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.	
 
-      Developer notes: since summons spells are bugged or unimplemented in arcemu
-                       (if unit is creature) we dont have more choice than spawn them
-                       manually... at least for now.
+	Developer notes: since summons spells are bugged or unimplemented in arcemu
+                     (if unit is creature) we dont have more choice than spawn them
+                     manually... at least for now.
 --]]
 
 --local FACTION_ALLIANCE = 12;
@@ -18,22 +26,18 @@ CYLINA_DARKHEART = {}
 
 function CYLINA_DARKHEART.OnSpawnAndDeath( unit, event )
 
+	-- on death
     if( event == 4 )
-
     then
-
         local minion = unit:GetCreatureNearestCoords( -9466, -6.72, 49.79, 12922 );
-
+		
         if( minion ~= nil )
-
         then
-
             minion:Despawn( 1000, 0 );
-
         end
 
-    elseif( event == 18 )
-
+	-- on spawn
+	elseif( event == 18 )
     then
         -- unit:CastSpell( 11939 ); -- cast "Summon Imp" (bugged spell on creature cast)
 
@@ -41,14 +45,13 @@ function CYLINA_DARKHEART.OnSpawnAndDeath( unit, event )
 
         local guid = unit:GetGUID();
 
+		-- set summoned by
         minion:SetUInt64Value( 0x0006 + 0x0008, guid );
-
+		
+		-- set created by
         minion:SetUInt64Value( 0x0006 + 0x000A, guid );
-
     end
-
 end
 
 RegisterUnitEvent( 6374, 4, CYLINA_DARKHEART.OnSpawnAndDeath );
-
 RegisterUnitEvent( 6374, 18, CYLINA_DARKHEART.OnSpawnAndDeath );
