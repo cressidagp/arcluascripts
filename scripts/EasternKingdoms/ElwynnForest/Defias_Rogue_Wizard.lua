c256d59c092e6d4118fe83c758e63d2b0b40b0b1
cressidagp <gcpigni@gmail.com>
2021-01-15 18:25:39 -0300

    Minor changes to Defias Rogue Wizard script.

--- scripts/EasternKingdoms/ElwynnForest/Defias_Rogue_Wizard.lua
+++ scripts/EasternKingdoms/ElwynnForest/Defias_Rogue_Wizard.lua
@@ -1,39 +1,37 @@
---[[ 
+--[[
 	ArcLuaScripts for ArcEmu
 	www.ArcEmu.org
-	Elwynn Forest: Defias Rogue Wizard
-	Engine: A.L.E
-	Credits: nil
+	Engine: A.L.E	  
+
+	Zone: Elwynn Forest
+	Creature: Defias Rogue Wizard (474)
+
+ 	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
+
 --]]
 
 DEFIAS_ROGUE_WIZARD = {}
 
 function DEFIAS_ROGUE_WIZARD.OoCCastBuff( unit, event )
 
-    if( event == 18 )
-	
+	-- on ai update
+    if( event == 21 )
     then
-	
-        local n = math.random( 1, 5 );
-		
-        unit:RegisterAIUpdateEvent( n * 1000 );
-		
-    else
-	
         if( unit:IsInCombat() == false and unit:HasAura( 12544 ) == false )
-		
         then
-		
             unit:FullCastSpell( 12544 ); -- Frost Amor
-			
-            unit:ModifyAIUpdateEvent( 1800000 );
-			
-        end
-		
-    end
+            unit:ModifyAIUpdateEvent( 1800000 );	
+		end
 	
+	-- on spawn
+	else
+        local n = math.random( 3, 6 );
+        unit:RegisterAIUpdateEvent( n * 1000 );
+    end
 end
 
 RegisterUnitEvent( 474, 18, DEFIAS_ROGUE_WIZARD.OoCCastBuff );
-
 RegisterUnitEvent( 474, 21, DEFIAS_ROGUE_WIZARD.OoCCastBuff );
