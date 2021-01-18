62e52c3dd71c60dc4db0545812b11d983039e9ba
cressidagp <gcpigni@gmail.com>
2021-01-18 02:07:02 -0300

    Minor changes to General Marcus Jonathan script.

--- scripts/EasternKingdoms/ElwynnForest/StormwindCity/General_Marcus_Jonathan.lua
+++ scripts/EasternKingdoms/ElwynnForest/StormwindCity/General_Marcus_Jonathan.lua
@@ -1,36 +1,42 @@
 --[[  
 	ArcLuaScripts for ArcEmu
 	www.ArcEmu.org
-	Stormwind City: General Marcus Jonathan
-	Engine: A.L.E
-	Credits: nil
+	Engine: A.L.E	
+
+	Zone: Stormwind City
+	Creature: General Marcus Jonathan (466)
+	
+	Credits:
+	
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
 	
 	enUS: "Greetings citizen."
+	
 	esMX: "Saludos ciudadano."
 	
 --]]
 
-local EMOTE_ONESHOT_WAVE = 3;
-local EMOTE_ONESHOT_SALUTE = 66;
+--local EMOTE_ONESHOT_WAVE = 3;
+--local EMOTE_ONESHOT_SALUTE = 66;
 
-MARCUS_JONATHAN = {}
+GENERAL_MARCUS_JONATHAN = {}
 
-function MARCUS_JONATHAN.OnEmote( unit, event, plr, eID )
+function GENERAL_MARCUS_JONATHAN.OnEmote( unit, _, plr, emoteId )
 
-    if( eID == EMOTE_ONESHOT_WAVE and unit:IsHostile( plr ) == false )
-	
-    then
+	if( unit:IsHostile( plr ) == false )
+	then
+		if( emoteId == 3 )	
+		then
+			unit:SendChatMessage( 12, 7, "Greetings citizen." );
 
-        unit:SendChatMessage( 12, 7, "Greetings citizen." );
-
-    elseif( eID == EMOTE_ONESHOT_SALUTE and unit:IsHostile( plr ) == false )
-	
-    then
-	
-        unit:Emote( EMOTE_ONESHOT_SALUTE, 0 );
-		
-    end
-	
+		elseif( emoteId == 66 )	
+		then	
+			unit:Emote( 66, 0 );
+		end
+    end	
 end
 
-RegisterUnitEvent( 466, 22, MARCUS_JONATHAN.OnEmote );
+RegisterUnitEvent( 466, 22, GENERAL_MARCUS_JONATHAN.OnEmote );
