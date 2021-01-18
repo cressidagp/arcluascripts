0f88d6469625231679daf7cacac6dea595609e42
cressidagp <gcpigni@gmail.com>
2021-01-17 21:16:19 -0300

    Minor changes to dummy: Force Target Salute.

--- scripts/DummySpells/6245.lua
+++ scripts/DummySpells/6245.lua
@@ -1,39 +1,41 @@
 --[[
 	ArcLuaScripts for ArcEmu
 	www.ArcEmu.org
-	Force Target Salute (6245)
 	Engine: A.L.E
-
+	
+	Dummy: Force Target Salute (6245)
+	
 	Credits:
-
+	
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
 	*) Hypersniper for his lua guides and some job in the lua engine.
 	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
-	*) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
 
 	Precondition(s):
-	
+
 	*) Need a target.
-	
+
 	Effect(s):
-	
+
 	*) Makes unit salute the target.
 	*) The target salutes back unit.
-	
+
 --]]
 
-function ForceTargetSaluteDummy( effectIndex, spellObject )
+FORCE_TARGET_SALUTE = {}
+
+function FORCE_TARGET_SALUTE.DummyHandler( _, spellObject )
 
 	local caster = spellObject:GetCaster();
-	
-	if( caster == nil ) then return; end
-	
+
 	local target = spellObject:GetTarget();
-	
+
 	if( target == nil ) then return; end
-	
+
 	caster:Emote( 66, 0 );
 	target:Emote( 66, 0 );
-	
+
 end
 
-RegisterDummySpell( 6245, ForceTargetSaluteDummy );
+RegisterDummySpell( 6245, FORCE_TARGET_SALUTE.DummyHandler );
