e188993b19d149afa4b0d52f301734ce8b6a3e15
cressidagp <gcpigni@gmail.com>
2021-01-18 02:19:15 -0300

    Rewrited Major Mattingly script to use just one function.

--- scripts/EasternKingdoms/ElwynnForest/StormwindCity/Major_Mattingly.lua
+++ scripts/EasternKingdoms/ElwynnForest/StormwindCity/Major_Mattingly.lua
@@ -1,23 +1,42 @@
+--[[
+	ArcLuaScripts for ArcEmu
+	www.ArcEmu.org
+	Engine: A.L.E	
+	
+	Zone: Stormwind City
+	Creature: Major Mattingly (14394)
+	
+	Credits:
+	
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
+	
+--]]
 
-local SPELL_RALLYNG_CRY = 22888; -- Rallyng Cry of the Dragonslayer
+--local SPELL_RALLYNG_CRY = 22888; -- Rallyng Cry of the Dragonslayer
 
+--[[
 local TEXT = { -- Yell, Yell, and them spawn onyxia head
 [ 1 ] = "Behold the might of the Alliance! The dread lady, Onyxia, hangs from the arches! Let the rallying cry of the dragon slayer lift your spirits!";
 [ 2 ] = "Citizens and allies of Stormwind, on this day, history has been made. <name> has laid waste to that which had attempted to usurp the rule of the kingdom. Gather round and join me in honoring our heroes.";
 };
+--]]
 
 -- Start:
-local QUEST_1 = 7497; -- The Journey Has Just Begun
+--local QUEST_1 = 7497; -- The Journey Has Just Begun
 
 -- End:
-local QUEST_2 = 7496; -- Celebrating Good Times
-local QUEST_3 = 7497;
-
+--local QUEST_2 = 7496; -- Celebrating Good Times
+--local QUEST_3 = 7497;
+print( "Lua memory used before: "..gcinfo().." Kb." );
 MAJOR_MATTINGLY = {}
 
 function MAJOR_MATTINGLY.CheckFriendly( unit )
+
 	local target = unit:GetClosestPlayer();
-	if( target ~= nil )
+	if( target )
 	then
 		if( target:GetStanding( 12 ) >= 0 )
 		then
@@ -34,3 +53,4 @@ function MAJOR_MATTINGLY.OnSpawn( unit )
 end
 
 RegisterUnitEvent( 14394, 18, MAJOR_MATTINGLY.OnSpawn );
+print( "Lua memory used after: "..gcinfo().." Kb." );
