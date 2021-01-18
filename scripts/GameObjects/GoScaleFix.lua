bcecd7e59cbb6ebf23b9f2e0a0ac8b918a3505c8
cressidagp <gcpigni@gmail.com>
2021-01-18 12:03:33 -0300

    Minor changes to gameobject scale fix script.

--- scripts/GameObjects/GoScaleFix.lua
+++ scripts/GameObjects/GoScaleFix.lua
@@ -1,35 +1,45 @@
---[[  
-	ArcLuaScripts for ArcEmu
-	www.ArcEmu.org
-	Gameobject scale fix: Farm Chicken Egg, Silithyst Mound
+--[[
+    ArcLuaScripts for ArcEmu
+    www.ArcEmu.org
 	Engine: A.L.E
-	Credits: nil
+	
+    Gameobject scale fix: Farm Chicken Egg, Silithyst Mound
+	
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
 --]]
 
 GO_SCALE_FIX = {}
 
-function GO_SCALE_FIX.OnSpawn( go, event )
+function GO_SCALE_FIX.OnSpawn( go )
 
+    -- Case: Farm Chicken Egg
+	
     if( go:GetEntry() == 161513 )
-	
     then
+		if( go:GetScale ~= 0.2 )
+		then
+			if( go:GetScale() == 0.2 )
+			then
+				go:SetScale( 0.2 );
+			end
+        end
+
+    -- Case: Silithyst Mound
 	
-        -- Case: Farm Chicken Egg
-
-        go:SetScale( 0.2 );
-
     elseif( go:GetEntry() == 181597 )
-	
     then
-	
-        -- Case: Silithyst Mound
-
-        go:SetScale( 0.3 );
-		
+		if( go:GetScale() ~= 0.3 )
+		then
+			if( go:GetScale() == 0.3 )
+			then
+				go:SetScale( 0.3 );
+			end
+        end
     end
-	
 end
 
 RegisterGameObjectEvent( 161513, 2, GO_SCALE_FIX.OnSpawn );
-
 RegisterGameObjectEvent( 181597, 2, GO_SCALE_FIX.OnSpawn );
