a17bf0a6f5d5e2e792bf0f5c57752eb3c7f8b4f9
cressidagp <gcpigni@gmail.com>
2021-01-18 12:57:05 -0300

    Minor changes to ArcLuaScript.

--- scripts/zArcLuaScripts.lua
+++ scripts/zArcLuaScripts.lua
@@ -1,10 +1,16 @@
 --[[
-      ArcLuaScripts for ArcEmu
-      www.ArcEmu.org
-      Engine: A.L.E
-      Credits: nil
+	ArcLuaScripts for ArcEmu
+	www.ArcEmu.org
+	Engine: A.L.E
+	
+	Credits:
+	
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
 
-      Developer notes: this beauty will check if we apply the newest arcluascripts.sql
+	Developer notes: this beauty will check if we apply the newest arcluascripts.sql
 
 --]]
 
