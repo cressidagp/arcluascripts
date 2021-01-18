f82ad2463133691d0c9d16f754636ab94c4c3485
cressidagp <gcpigni@gmail.com>
2021-01-18 12:42:06 -0300

    Minor changes to Vizzklick script.

--- scripts/Kalimdor/Tanaris/Vizzklick.lua
+++ scripts/Kalimdor/Tanaris/Vizzklick.lua
@@ -1,21 +1,27 @@
 --[[
 	ArcLuaScripts for ArcEmu
 	www.ArcEmu.org
-	Tanaris: Vizzklick
 	Engine: A.L.E
 
-	Credits:
+	Zone: Tanaris
+	Creature: Vizzklick (6568)
 
-	*) Trinity for texts.
+	Credits:
+	
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
 	*) Hypersniper for his lua guides and some job in the lua engine.
 	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
-	*) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
+
+	enUS locale: "I wish to browse your wares."
+	
+	esMX locale: "Me gustaría revisar tus productos."
 
 --]]
 
 VIZZKLICK = {}
 
-function VIZZKLICK.OnHelloOnSelect( unit, event, plr, id, selection, code )
+function VIZZKLICK.OnHelloOnSelect( unit, event, plr, id, selection )
 
 	if( event == 1 )
 	then
@@ -32,3 +38,4 @@ end
 
 RegisterUnitGossipEvent( 6568, 1, VIZZKLICK.OnHelloOnSelect );
 RegisterUnitGossipEvent( 6568, 2, VIZZKLICK.OnHelloOnSelect );
+
