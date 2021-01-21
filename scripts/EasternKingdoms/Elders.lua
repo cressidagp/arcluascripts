adfc793bbb67c98fb93a95e9cd414b89029f247b
cressidagp <gcpigni@gmail.com>
2021-01-20 22:14:24 -0300

    Minor changes to Elders script.

--- scripts/EasternKingdoms/Elders.lua
+++ scripts/EasternKingdoms/Elders.lua
@@ -1,16 +1,24 @@
 --[[  
 	ArcLuaScripts for ArcEmu
 	www.ArcEmu.org
-	Elwynn Forest and Tirisfall Glades: Elders
 	Engine: A.L.E
-	Credits: nil
 	
-	enUS:
+	Zone: Elwynn Forest and Tirisfall Glades
+	Creature: Elders
+	
+	Credits:
+
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
+	
+	enUS locale:
 	
 	[ 1 ] = "I would like to whisper my secret code to you to receive Tyrael's Hilt.";
 	[ 2 ] = "You're secret code is not a valid one...";
 	
-	esMX:
+	esMX locale:
 	
 	[ 1 ] = "Me gustaría susurrarte mi código secreto para que reciba Empuñadura de Tyrael.";
 	[ 2 ] = "Tu código secreto no es válido...";
@@ -28,40 +36,31 @@ ELDERS = {}
 
 function ELDERS.OnHelloOnSelect( unit, event, plr, id, selection, code )
 
-		if( event == 1 )
-		
+	if( event == 1 )
+	then
+		unit:GossipCreateMenu( 13441, plr, 0 );
+		unit:GossipMenuAddItem( 0, CHAT[ event ], 2, 1, "", 0 );
+		unit:GossipSendMenu( plr );
+	else
+		if( selection == 2 )
 		then
-				unit:GossipCreateMenu( 13441, plr, 0 );
-				
-				unit:GossipMenuAddItem( 0, CHAT[ 1 ], 2, 1, "", 0 );
-				
-				unit:GossipSendMenu( plr );
+			if( code == ""..SECRET.."" )
+			then
+				plr:AddItem( 39656, 1 );
+			else
+				unit:SendChatMessage( 12, 0, CHAT[ event ] );
+			end
+			
+			plr:GossipComplete();	
 
-		else
-				if( selection == 2 )
-				
-				then
-						if( code == ""..SECRET.."" )
-						
-						then
-								plr:AddItem( 39656, 1 );
-						else
-								unit:SendChatMessage( 12, 0, CHAT[ 2 ] );
-						end
-						
-						plr:GossipComplete();	
-				end	
 		end	
+	end	
 end
 
 -- Stormwind: Ian Drake:
-
 RegisterUnitGossipEvent( 29093, 1, ELDERS.OnHelloOnSelect );
-
 RegisterUnitGossipEvent( 29093, 2, ELDERS.OnHelloOnSelect );
 
 -- Undercity: Edward Cairn:
-
 RegisterUnitGossipEvent( 29095, 1, ELDERS.OnHelloOnSelect );
-
 RegisterUnitGossipEvent( 29095, 2, ELDERS.OnHelloOnSelect );
