e3c9a47343f79eb7e06a40611e09fbd0e5722128
cressidagp <gcpigni@gmail.com>
2021-01-21 19:55:50 -0300

    Added Marshal Isildor script.

---
+++ scripts/Outland/HellfirePeninsula/Marshal_Isildor.lua
@@ -0,0 +1,60 @@
+--[[
+	ArcLuaScripts for ArcEmu
+	www.ArcEmu.org
+	Engine: A.L.E
+	
+	Zone: Hellfire Peninsula
+	Creature: Marshall Isildor
+
+	Credits:
+
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
+	
+	enUS locale:
+	
+	[ 1 ] = "What could be taking so long?",
+	[ 2 ] = "How long can it take to pick a handful of weeds?",
+	[ 3 ] = "At this rate I could have gathered them myself!",
+	[ 4 ] = "If you want something done right, do it yourself!",
+	
+	esMX locale:
+
+--]]
+
+--local NPC_MARSHAL_ISILDOR = 19308;
+
+local chat = {
+[ 1 ] = "This war - and don't mistake this for anything but a war - is not going to win itself. If we do not fight, scratch, claw, and kick for every speck of land on this shattered world, all will be lost.",
+[ 2 ] = "How many more soldiers need to die before we start paying attention out there?",
+[ 3 ] = "Do you think that demons will play fair? These beasts are more savage than orcs and more cunning than the undead. Do not attempt to engage Legion by normal standards of war.",
+[ 4 ] = "I regret to inform you all that we sent home another ten soldiers today - in body bags.",
+};
+
+MARSHAL_ISILDOR = {}
+
+function MARSHAL_ISILDOR.OoCRandomChat( unit )
+
+	if( unit:IsInCombat() == false )
+	then
+		local rand = math.random( 1, 4 );
+		if( rand <= 1 and rand <= 3 )
+		then
+			unit:SendChatMessage( 12, 7, chat[ rand ] );
+			unit:Emote( 5, 0 );
+		else
+			unit:SendChatMessage( 12, 7, chat[ rand ] );
+			unit:Emote( 1, 0 );
+		end
+	end
+end
+
+function MARSHAL_ISILDOR.OnSpawn( unit )
+
+	unit:RegisterEvent( MARSHAL_ISILDOR.OoCRandomChat, 60000, 1 );
+	unit:RegisterEvent( MARSHAL_ISILDOR.OoCRandomChat, math.random( 120000, 180000 ), 0 );
+end
+
+RegisterUnitEvent( 19308, 18, MARSHAL_ISILDOR.OnSpawn );
