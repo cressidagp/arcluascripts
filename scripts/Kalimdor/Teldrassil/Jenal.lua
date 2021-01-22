54b97e41a1f08e8b2d6f19ceb24ec82925e1baa4
cressidagp <gcpigni@gmail.com>
2021-01-22 16:22:06 -0300

    Added Jenal gossip script.

---
+++ scripts/Kalimdor/Teldrassil/Jenal.lua
@@ -0,0 +1,48 @@
+--[[
+	ArcLuaScripts for ArcEmu
+	www.ArcEmu.org
+	Engine: A.L.E
+	
+	Zone: Darnassus
+	Creature: Jenal
+
+	Credits:
+
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
+
+	enUS locale:
+
+	esMX locale:
+
+--]]
+
+--local NPC_JENAL = 9047;
+
+JENAL = {}
+
+function JENAL.OnHelloSelect( unit, event, plr, id, selection )
+
+	if( event == 1 )
+	then
+		unit:GossipCreateMenu( 2313, plr, 0 );
+		unit:GossipMenuAddItem( 0, "What are you doing out here?", 1, 0 );
+		unit:GossipAddQuests( plr );
+		
+	else
+	
+		if( selection == 1 )
+		then
+			unit:GossipCreateMenu( 2314, plr, 0 );
+
+		end
+	end		
+	unit:GossipSendMenu( plr );
+end
+
+RegisterUnitGossipEvent( 9047, 1, JENAL.OnHelloSelect );
+RegisterUnitGossipEvent( 9047, 2, JENAL.OnHelloSelect );
+
+
