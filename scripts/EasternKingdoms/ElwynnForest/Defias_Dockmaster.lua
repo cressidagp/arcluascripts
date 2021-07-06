90edc66d03af3ce062e13f1b10807e6e86220a54
cressidagp <gcpigni@gmail.com>
2021-07-05 23:43:31 -0300

    Minor changes to Defias Dockmaster script.

--- scripts/EasternKingdoms/ElwynnForest/Defias_Dockmaster.lua
+++ scripts/EasternKingdoms/ElwynnForest/Defias_Dockmaster.lua
@@ -20,18 +20,29 @@ DEFIAS_DOCKMASTER = {};
 
 function DEFIAS_DOCKMASTER.OnCombat( unit, event )
 
+	--
+	-- on ai update
+	--
+
 	if( event == 21 )
 	then
 		DEFIAS_DOCKMASTER.hasSummoned = false;
+
+	--
+	-- on combat
+	--
 	
 	else
+
 		if( DEFIAS_DOCKMASTER.hasSummoned == false )
 		then
 			local bodyguard1 = unit:SpawnCreature( 6866, -9967.55, -135.956, 24.5909, 0.170326, 14, 60000, 2023, 0, 0, 1, 0 );
 			local bodyguard2 = unit:SpawnCreature( 6866, -9958.49, -140.526, 24.2409, 4.0015, 14, 60000, 2023, 0, 0, 1, 0 );
 			local bodyguard3 = unit:SpawnCreature( 6866, -9964.59, -140.567, 24.5105, 0.741307, 14, 60000, 2023, 0, 0, 1, 0 );
-			unit:RegisterAIUpdateEvent( 60000 );
+
 			DEFIAS_DOCKMASTER.hasSummoned = true;
+
+			unit:RegisterAIUpdateEvent( 60000 );	
 		end
 	end
 end
