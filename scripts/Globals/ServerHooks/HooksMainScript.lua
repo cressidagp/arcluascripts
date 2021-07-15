7881209b10d33719c3f41bbc13ba1066f549b4ff
cressidagp <gcpigni@gmail.com>
2021-07-15 13:55:25 -0300

    Server Hook main script now fix players start orientation.

--- scripts/Globals/ServerHooks/HooksMainScript.lua
+++ scripts/Globals/ServerHooks/HooksMainScript.lua
@@ -1,20 +1,25 @@
 --[[
+
 	ArcLuaScripts for ArcEmu
 	www.ArcEmu.org
-	Hooks Main script
 	Engine: A.L.E
-
-	Credits:
-
-	*) Hypersniper for his lua guides and some job in the lua engine.
-	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
-	*) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
 	
+	Server Hooks: Main Script
+
 	Features:
 	
 	*) Add gm items to new characters.
 	*) Add "Heroic Presence" aura for Draneis players.
 	*) Cast "Blood Presence" and add "Undying Resolve" for Death Knights.
+	*) Fix character start orientation since arcemu cant handle it.
+
+	Credits:
+
+	*) TrinityCore for texts, sound ids, timers, spell ids and some inspiration.
+	*) DarkAngel39 for his instance progression system.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
 
 --]]
 
@@ -33,12 +38,44 @@ function HOOKS.Consolidated( event, plr )
 				plr:AddItem( GM_ITEMS[ i ], 1 );
 			end
 		end
+		
+		if( plr:GetPlayerClass() ~= "Death Knight" )
+		then
+			local race = plr:GetPlayerRace();
+			
+			if( race == 1 or race == 2 or race == 6 or race == 7 or race == 8 ) then return; end
+			
+			if( race == 3 )
+			then
+				plr:SetFacing( 6.17716 );
+				
+			elseif( race == 4 )
+			then
+				plr:SetFacing( 5.48033 );
+				
+			elseif( race == 5 )
+			then
+				plr:SetFacing( 2.70526 );
+				
+			elseif( race == 10 )
+			then
+				plr:SetFacing( 5.31605 );
+				
+			elseif( race == 11 )
+			then
+				plr:SetFacing( 2.08364 );
+			end
+
+		else
+			plr:SetFacing( 3.65997 );
+		end
 
 	elseif( event == 4 ) -- OnEnterWorld
 	then
 		if( plr:GetPlayerRace() == 11 )
 		then
 			local class = plr:GetPlayerClass();
+			
 			if( class == "Priest" or class == "Shaman" or class == "Mage" )
 			then
 				plr:AddAura( 28878, 0 ); -- Heroic Presence
@@ -50,6 +87,7 @@ function HOOKS.Consolidated( event, plr )
 		if( plr:GetPlayerClass() == "Death Knight" )
 		then
 			plr:CastSpell( 48266 ); -- Blood Presence
+			
 			if( plr:GetMapId() == 609 )
 			then
 				plr:AddAura( 51915, 0 ); -- Undying Resolve
