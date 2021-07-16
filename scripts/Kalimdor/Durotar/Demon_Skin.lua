939110c48704a25040d94194c188df6e40b05a45
cressidagp <gcpigni@gmail.com>
2021-07-16 14:23:45 -0300

    Added Frostmane Shadowcaster register to Demon_Skin file.

--- scripts/Kalimdor/Durotar/Demon_Skin.lua
+++ scripts/Kalimdor/Durotar/Demon_Skin.lua
@@ -1,10 +1,11 @@
---[[  
+--[[
+
 	ArcLuaScripts for ArcEmu
 	www.ArcEmu.org
 	Engine: A.L.E
 	
 	Zone: Durotar
-	Creatures: Demon Skin casters
+	Creature: Demon Skin casters
 
 	Credits:
 
@@ -17,6 +18,7 @@
 
 --local NPC_GAZZUZ = 3204;
 --local NPC_FELWEAVER_SCORN = 5822;
+--local NPC_FROSTMANE_SHADOWCASTER = 1124;
 
 --local SPELL_DEMON_SKIN = 20798;
 
@@ -25,23 +27,24 @@ OOC_DEMON_SKIN = {};
 function OOC_DEMON_SKIN.CastBuff( unit, event )
 
 	-- on ai update
-    if( event == 21 )
-    then
-        if( unit:IsInCombat() == false and unit:HasAura( 20798 ) == false )
-        then
-            unit:FullCastSpell( 20798 );
-            unit:ModifyAIUpdateEvent( 1800000 );	
+	if( event == 21 )
+	then
+		if( unit:IsInCombat() == false and unit:HasAura( 20798 ) == false )
+		then
+			unit:FullCastSpell( 20798 );
+			unit:ModifyAIUpdateEvent( 1800000 );	
 		end
 	
 	-- on spawn
 	else
-	
-        local n = math.random( 3, 5 );
-        unit:RegisterAIUpdateEvent( n * 1000 ); 	
-    end	
+		local n = math.random( 3, 5 );
+		unit:RegisterAIUpdateEvent( n * 1000 ); 	
+	end	
 end
 
 RegisterUnitEvent( 3204, 18, OOC_DEMON_SKIN.CastBuff );
 RegisterUnitEvent( 3204, 21, OOC_DEMON_SKIN.CastBuff );
 RegisterUnitEvent( 5822, 18, OOC_DEMON_SKIN.CastBuff );
 RegisterUnitEvent( 5822, 21, OOC_DEMON_SKIN.CastBuff );
+RegisterUnitEvent( 1124, 18, OOC_DEMON_SKIN.CastBuff );
+RegisterUnitEvent( 1124, 21, OOC_DEMON_SKIN.CastBuff );
