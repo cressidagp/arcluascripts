ce2554f6a494e342e23a171f2b9abef842bf51c2
cressidagp <gcpigni@gmail.com>
2023-06-17 22:00:49 -0300

    Expanded serverhook on quest complete with quests 823, 806

--- scripts/Globals/ServerHooks/HooksOnQuest.lua
+++ scripts/Globals/ServerHooks/HooksOnQuest.lua
@@ -10,22 +10,27 @@
   *) Hypersniper for his lua guides and some job in the lua engine.
   *) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
   *) ArcEmu developers for ArcEmu and his ArcEmu Lua Engine, specially to dfighter1985.
-  
-  enUS: "Welcome to Honor Hold, "..plr:GetName()..".  It's good to have you."
-  
-  esMX: "Bienvenido a Honor Hold, "..plr:GetName()..". Es bueno tenerte."
 
 --]]
 
-HOOKS_QUEST = {};
+HOOKS_QUEST = {}
 
-function HOOKS_QUEST.OnComplete( event, plr, questID, questEnder )
+function HOOKS_QUEST.OnComplete( event, plr, questId, questEnder )
 
-    if( questID == 10254 ) -- Force Commander Danath
-    then
-      questEnder:SendChatMessage( 12, 0, "Welcome to Honor Hold, "..plr:GetName()..".  It's good to have you." );
-      questEnder:CastSpellOnTarget( 6245, plr );
-    end
+	if questId == 823 then
+		questEnder:SendChatMessage( 16, 0, "%s listens to $n's report..." )
+		questEnder:EventChat( 12, 1, What??  The Burning Blade is spreading!  We must investigate!, 2000 )
+	end
+	
+	if questId == 806 then
+		questEnder:SendChatMessage( 16, 0, "We will suffer no demon's servant in our lands!" )
+		questEnder:EventChat( 12, 1, What??  The Burning Blade is spreading!  We must investigate!, 2000 )
+	end
+		
+    if questId == 10254 then
+		questEnder:SendChatMessage( 12, 0, "Welcome to Honor Hold, "..plr:GetName()..".  It's good to have you." )
+		questEnder:CastSpellOnTarget( 6245, plr );
+	end
 end
 
 RegisterServerHook( 22, HOOKS_QUEST.OnComplete );
