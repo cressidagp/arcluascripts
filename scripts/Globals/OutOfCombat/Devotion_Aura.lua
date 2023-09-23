2fc878833daa37f3e0703d734dc656f62b96b25b
cressidagp <gcpigni@gmail.com>
2023-09-23 15:08:43 -0300

    Added a script for creatures wich cast 'Devotion Aura' when OOC.

---
+++ scripts/Globals/OutOfCombat/Devotion_Aura.lua
@@ -0,0 +1,52 @@
+--[[
+
+	ArcLuaScripts for ArcEmu
+	www.ArcEmu.org
+	Engine: A.L.E
+	
+	Zone: Global
+	Creatures: Devotion Aura casters
+
+	Credits:
+	
+	*) TrinityCore for texts, sound ids, timers, spell ids and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and his A.L.E, specially to dfighter1985.
+	
+--]]
+
+--local NPCID_CAPTAIN_MALRACHE = 1665
+
+--local SPELL_DEVOTION_AURA = 8258
+
+OOC_DEVOTION_AURA = {}
+
+function OOC_DEVOTION_AURA.CastBuff( unit, event )
+
+	--
+	-- on ai update
+	--
+	
+    if event == 21 then
+	
+        if unit:IsInCombat() == false and unit:HasAura( 8258 ) == false then
+		
+            unit:FullCastSpell( 8258 )
+            unit:ModifyAIUpdateEvent( 240000 )
+			
+        end
+	
+	--
+	-- on spawn
+	--
+	
+    else
+		local n = math.random( 3, 5 )
+		unit:RegisterAIUpdateEvent( n * 1000 )
+    end
+end
+
+-- Captain Melrache:
+RegisterUnitEvent( 1665, 18, OOC_DEVOTION_AURA.CastBuff );
+RegisterUnitEvent( 1665, 21, OOC_DEVOTION_AURA.CastBuff );
