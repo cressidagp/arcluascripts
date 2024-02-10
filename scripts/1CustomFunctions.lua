7d0a448fdaa77424f4e43b12a8cbecbb53cfc13e
cressidagp <cressidagp@gmail.com>
2024-02-10 18:57:41 -0300

    Added script with custom functions.

---
+++ scripts/1CustomFunctions.lua
@@ -0,0 +1,27 @@
+--[[
+
+	ArcLuaScripts for ArcEmu
+	Engine: A.L.E
+	2024
+
+	Credits:
+
+	*) TrinityCore for texts, sound ids, timers and some Inspiration.
+	*) Hypersniper for his lua guides and some job in the lua engine.
+	*) Paroxysm for his Modular Way of scripting, LCF and Lua Scripting Expected Standards.
+	*) ArcEmu developers for ArcEmu and A.L.E, specially to dfighter1985.
+
+	Developer notes: added here some custom function(s) that can be used at more than one script.
+
+--]]
+
+function table.find( t, v ) 
+    if type( t ) == "table" and v then
+        for k, value in pairs( t ) do
+            if v == value then
+                return true 
+            end
+        end
+    end
+    return false
+end
