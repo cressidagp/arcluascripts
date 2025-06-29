dd955cf058ac35f302b25fb008c6f23f0bb8a656
cressidagp <cressidagp@gmail.com>
2025-06-29 00:19:50 -0300

    Lord Marrowgar will try to cast bonestorm now.

--- scripts/prod/Northrend/IcecrownCitadel/Lord_Marrowgar.lua
+++ scripts/prod/Northrend/IcecrownCitadel/Lord_Marrowgar.lua
@@ -3,49 +3,19 @@
 	www.ArcEmu.org
 	Icecrown Citadel: Lord Marrowgar
 	Engine: A.L.E
-	Credits: Trinity for sound ids, chats and text.
 
-local SOUND = {
-[ 1 ] = 16950; -- OnEnterZone
-[ 2 ] = 16941; -- OnEnterCombat
-[ 3 ] = 16946; -- OnCast: "Bonestorm"
-[ 4 ] = 16947; -- OnCast: "Bonespike" 1
-[ 5 ] = 16948; -- OnCast: "Bonespike" 2
-[ 6 ] = 16949; -- OnCast: "Bonespike" 3
-[ 7 ] = 16942; -- OnTargetDied 1
-[ 8 ] = 16943; -- OnTargetDied 2
-[ 9 ] = 16944; -- OnDeath
-[ 10 ] = 16945; -- OnCast: "Berserk"
-};
-
-local CHAT = {
-[ 1 ] = "This is the beginning AND the end, mortals. None may enter the master's sanctum!";
-[ 2 ] = "The Scourge will wash over this world as a swarm of death and destruction!";
-[ 3 ] = "BONE STORM!";
-[ 4 ] = "Bound by bone!";
-[ 5 ] = "Stick around!";
-[ 6 ] = "The only escape is death!";
-[ 7 ] = "More bones for the offering!";
-[ 8 ] = "Languish in damnation!";
-[ 9 ] = "I see... Only darkness.";
-[ 10 ] = "THE MASTER'S RAGE COURSES THROUGH ME!";
-};
-
-local TEXT = {
-[ 1 ] = "%s creates a whirling storm of bone!"; -- Bonestorm emote
-};
 --]]
 
 local self = getfenv( 1 )
 
-function LordMarrowgar_OnSpawn( unit, event )
+function LordMarrowgar_onSpawn( unit, event )
 
-	unit:SendChatMessage( 14, 0, "This is the beginning AND the end, mortals. None may enter the master's sanctum!" )
 	unit:PlaySoundToSet( 16950 )
+	unit:SendChatMessage( 14, 0, "This is the beginning AND the end, mortals. None may enter the master's sanctum!" )
 
 end
 
-function LordMarrowgar_OnEnterCombat( unit, event, attacker )
+function LordMarrowgar_onEnterCombat( unit, event, attacker )
 
 	unit:PlaySoundToSet( 16941 )
 	
@@ -58,41 +28,53 @@ function LordMarrowgar_OnEnterCombat( unit, event, attacker )
 		boneSliceAttack = 10,
 		coldFlame = 5,
 		boneSpikeGraveyard = 15,
-		coldflame = 5,
 		warnBoneStorm = math.random( 45, 50 ),
+		boneStormBegin = nil,
+		boneStormMove = nil,
+		boneStormEnd = nil,
 		berserk = 10 * 60 * 1000
 	
 	}
 
 end
 
-function LordMarrowgar_OnTargetDied( unit, event, victim )
+function LordMarrowgar_onTargetDied( unit, event, victim )
 
 	local chance = math.random( 0, 1 )
 	
 	if chance == 0 then
 	
-		unit:SendChatMessage( 14, 0, "More bones for the offering!" )
 		unit:PlaySoundToSet( 16942 )
-	
+		unit:SendChatMessage( 14, 0, "More bones for the offering!" )
+
 	else
 		
-		unit:SendChatMessage( 14, 0, "Languish in damnation!" )
 		unit:PlaySoundToSet( 16943 )
+		unit:SendChatMessage( 14, 0, "Languish in damnation!" )
 	
 	end
 	
 end
 
-function LordMarrowgar_OnDied( unit, event, killer )
+function LordMarrowgar_onDied( unit, event, killer )
 
 	unit:PlaySoundToSet( 16944 )
 
 end
 
-function LordMarrowgar_OnAIUpdate( unit )
+function LordMarrowgar_onAIUpdate( unit )
 
-	if( unit:GetAIState() == 2 ) then return; end
+	-- dont bother with this function if boss dont have target
+	if unit:GetNextTarget() == nil then
+		unit:WipeThreatList()
+		return 
+	end
+	
+	
+	-- dont bother with this function if boss its casting
+	if unit:GetAIState() == 2 then 
+		return
+	end
 
 	local vars = self[ tostring( unit ) ]
 	
@@ -121,67 +103,92 @@ function LordMarrowgar_OnAIUpdate( unit )
 	end
 	
 	vars.boneSpikeGraveyard = vars.boneSpikeGraveyard - 1
-	vars.coldflame = vars.coldflame - 1
-	vars.warnBonestorm = vars.warnBonestorm - 1
+	vars.coldFlame = vars.coldFlame - 1
+	vars.warnBoneStorm = vars.warnBoneStorm - 1
 	vars.berserk = vars.berserk - 1
+	
+	if vars.boneStormBegin ~= nil then vars.boneStormBegin = vars.boneStormBegin - 1 end
+	if vars.boneStormMove ~= nil then vars.boneStormMove = vars.boneStormMove - 1 end
+	if vars.boneStormEnd ~= nil then vars.boneStormEnd = vars.boneStormEnd -1 end
 	
 	if vars.boneSpikeGraveyard <= 0 then
 	
-		unit:FullCastSpell( 69057 )
+		-- dont cast if boss has bonestorm aura
+		if unit:HasAura( 69076 ) == false then
+	
+			unit:FullCastSpell( 69057 )
 		
-		local chance = math.random( 0, 2 )
+			local chance = math.random( 0, 2 )
 		
-		if chance == 0 then
+			if chance == 0 then
 		
-			unit:SendChatMessage( 14, 0, "Bound by bone!" )
-			unit:PlaySoundToSet( 16947 )
+				unit:PlaySoundToSet( 16947 )
+				unit:SendChatMessage( 14, 0, "Bound by bone!" )
 		
-		elseif chance == 1 then
+			elseif chance == 1 then
 		
-			unit:SendChatMessage( 14, 0, "Stick around!" )
-			unit:PlaySoundToSet( 16948 )
+				unit:PlaySoundToSet( 16948 )
+				unit:SendChatMessage( 14, 0, "Stick around!" )
 		
-		else
+			else
 		
-			unit:SendChatMessage( 14, 0, "The only escape is death!" )
-			unit:PlaySoundToSet( 16949 )
+				unit:PlaySoundToSet( 16949 )
+				unit:SendChatMessage( 14, 0, "The only escape is death!" )
 		
-		end
+			end
+		
+		end 
 		
 		vars.boneSpikeGraveyard = math.random( 15, 20 )
 	
-	elseif vars.coldflame <= 0 then
+	elseif vars.coldFlame <= 0 then
 	
+		-- check for bonestorm aura
 		if unit:HasAura( 69076 ) == false then
 		
+			--- cast normal coldflame
 			unit:FullCastSpell( 69140 )
 			
 		else
 		
+			--- cast coldflame bonestorm
 			unit:FullCastSpell( 72705 )
 			
 		end
 		
-		vars.coldflame = 5
+		vars.coldFlame = 5
 	
-	elseif vars.warnBonestorm <= 0 then
+	elseif vars.warnBoneStorm <= 0 then
 	
 		unit:SendChatMessage( 42, 0, "%s creates a whirling storm of bone!" )
-		vars.warnBonestorm = 95
+		vars.warnBoneStorm = 95
+		vars.boneStormBegin = 3
+	
+	elseif vars.boneStormBegin ~= nil and vars.boneStormBegin <= 0 then
+		
+		unit:CastSpell( 69076 )
+		unit:PlaySoundToSet( 16946 )
+		unit:SendChatMessage( 14, 0, "BONE STORM!" )
+		vars.boneStormBegin = nil
+		vars.boneStormEnd = 60 + 1
+	
+	elseif vars.boneStormEnd ~= nil and vars.boneStormEnd <= 0 then
+	
+		vars.boneStormEnd = nil
 	
 	elseif vars.berserk <= 0 then
 	
 		unit:CastSpell( 26662 )
-		unit:SendChatMessage( 14, 0, "THE MASTER'S RAGE COURSES THROUGH ME!" )
 		unit:PlaySoundToSet( 16945 )
+		unit:SendChatMessage( 14, 0, "THE MASTER'S RAGE COURSES THROUGH ME!" )
 		vars.berserk = 10 * 60 * 1000
 	
 	end
 
 end
 
-RegisterUnitEvent( 36612, 18, LordMarrowgar_OnSpawn )
-RegisterUnitEvent( 36612, 1, LordMarrowgar_OnEnterCombat )
-RegisterUnitEvent( 36612, 3, LordMarrowgar_OnTargetDied )
-RegisterUnitEvent( 36612, 4, LordMarrowgar_OnDied )
-RegisterUnitEvent( 36612, 21, LordMarrowgar_OnAIUpdate )
+RegisterUnitEvent( 36612, 18, LordMarrowgar_onSpawn )
+RegisterUnitEvent( 36612, 1, LordMarrowgar_onEnterCombat )
+RegisterUnitEvent( 36612, 3, LordMarrowgar_onTargetDied )
+RegisterUnitEvent( 36612, 4, LordMarrowgar_onDied )
+RegisterUnitEvent( 36612, 21, LordMarrowgar_onAIUpdate )
