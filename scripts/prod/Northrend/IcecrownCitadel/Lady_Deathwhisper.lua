e795ed7effebf7a1108063d7c9adc3546864e248
cressidagp <cressidagp@gmail.com>
2025-07-15 10:08:44 -0300

    Started to work on Lady Deathwhisper.

---
+++ scripts/prod/Northrend/IcecrownCitadel/Lady_Deathwhisper.lua
@@ -0,0 +1,151 @@
+--[[
+
+PHASE_ALL       = 0
+PHASE_INTRO     = 1
+PHASE_ONE       = 2
+PHASE_TWO       = 3
+	
+--]]
+
+local self = getfenv( 1 )
+
+function LadyDeathwhisper_onSpawn( unit, event )
+
+	self[ tostring( unit ) ] = {
+	
+		waveCounter = 0,
+		nextVengefulShadeTargetGUID = 0,
+		cultistQueue = 0,
+		darnavanGUID = 0,
+		dominateMindCount = { 0, 1, 1, 3 },
+		action = 0,
+		chat = 0,
+		phase = 0
+		
+	
+	}
+	
+	unit:RegisterAIUpdateEvent( 1000 )
+
+end
+
+function LadyDeathwhisper_doAction( unit )
+
+	local vars = self[ tostring( unit ) ]
+	
+	local action = vars.action
+	
+	if action == 0 then
+	
+		unit:PlaySoundToSet( 17272 )
+	
+		unit:SendChatMessage( 14, 0, "You have found your way here, because you are among the few gifted with true vision in a world cursed with blindness." )
+		
+		vars.phase = 1
+		
+		vars.chat = 10
+		
+	elseif action == 1 then
+	
+		unit:PlaySoundToSet( 17273 )
+		
+		unit:SendChatMessage( 14, 0, "You can see through the fog that hangs over this world like a shroud, and grasp where true power lies." )
+		
+		vars.chat = 21
+	
+	elseif action == 3 then
+	
+		unit:PlaySoundToSet( 16878 )
+		
+		unit:SendChatMessage( 14, 0, "Fix your eyes upon your crude hands: the sinew, the soft meat, the dark blood coursing within." )
+		
+		vars.chat = 11
+		
+	elseif action == 4 then
+	
+		unit:PlaySoundToSet( 17268 )
+		
+		unit:SendChatMessage( 14, 0, "It is a weakness; a crippling flaw....  A joke played by the Creators upon their own creations." )
+		
+		vars.chat = 9
+		
+	elseif action == 5 then
+	
+		unit:PlaySoundToSet( 17269 )
+		
+		unit:SendChatMessage( 14, 0, "The sooner you come to accept your condition as a defect, the sooner you will find yourselves in a position to transcend it." )
+		
+		vars.chat = 21
+		
+	elseif action == 6 then
+	
+		unit:PlaySoundToSet( 17270 )
+		
+		unit:SendChatMessage( 14, 0, "Through our Master, all things are possible. His power is without limit, and his will unbending." )
+		
+		vars.chat = 10
+		
+	elseif action == 7 then
+	
+		unit:PlaySoundToSet( 17271 )
+		
+		unit:SendChatMessage( 14, 0, "Those who oppose him will be destroyed utterly, and those who serve -- who serve wholly, unquestioningly, with utter devotion of mind and soul -- elevated to heights beyond your ken." )
+		
+		vars.chat = nil
+		
+	end
+	
+	action = action + 1
+	
+	vars.action = action
+
+end
+
+function LadyDeathwhisper_onEnterCombat( unit, event, attacker )
+
+	
+
+end
+
+function LadyDeathwhisper_onAIUpdate( unit, event )
+
+	local vars = self[ tostring( unit ) ]
+	
+	if vars.phase == 0 then return end
+	
+	if vars.phase == 1 then
+	
+		if vars.chat ~= nil then vars.chat = vars.chat - 1 end
+		
+		if vars.chat <= 0 then
+		
+			LadyDeathwhisper_doAction( unit )
+		
+		end
+	
+	end
+	
+	if vars.phase == 2 then
+	
+		if unit:GetNextTarget() == nil then
+			unit:WipeThreatList()
+			return 
+		end
+	
+		vars.boneSlice = vars.boneSlice - 1
+		
+		if vars.boneSlice <= 0 then
+		
+			-- do sheet
+		
+		end
+	
+	end
+
+end
+
+
+
+RegisterUnitEvent( 36855, 18, LadyDeathwhisper_onSpawn )
+RegisterUnitEvent( 36855, 1, LadyDeathwhisper_onEnterCombat )
+RegisterUnitEvent( 36855, 21, LadyDeathwhisper_onAIUpdate )
