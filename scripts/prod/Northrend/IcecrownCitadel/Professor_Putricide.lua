ba379e4421a74151533662ff669041f07dabfa95
cressidagp <cressidagp@gmail.com>
2025-12-27 20:12:32 -0300

    More work on Professor Putricide script.

--- scripts/prod/Northrend/IcecrownCitadel/Professor_Putricide.lua
+++ scripts/prod/Northrend/IcecrownCitadel/Professor_Putricide.lua
@@ -1,6 +1,7 @@
 --[[
 
 local NPC_PROFESSOR_PUTRICIDE = 36678
+local VEHICLE_ID = 587
 
 enum PutricidePhases
 {
@@ -20,10 +21,10 @@ local SPELL_UNHOLY_INFUSION_CREDIT = 71518
 local SPELL_MUTATED_PLAGUE_CLEAR = 72618
 
 -- Festergut:
-SPELL_RELEASE_GAS_VISUAL = 69125
+local SPELL_RELEASE_GAS_VISUAL = 69125
 
 -- Shared spells:
-SPELL_BERSERK2 = 47008
+local SPELL_BERSERK2 = 47008
 
 --]]
 
@@ -53,6 +54,7 @@ function ProfessorPutricide_onEnterCombat( unit, event, attacker )
 	
 	local vars = self[ tostring( unit ) ]
 	
+	-- dont bother with this func on festergut and rotface phases
 	if vars.phase == 1 or vars.phase == 2 then
 		return
 	end
@@ -61,6 +63,7 @@ function ProfessorPutricide_onEnterCombat( unit, event, attacker )
 	vars.slimePuddle = 10
 	vars.unstableExperiment = math.random( 30, 35 )
 	
+	-- is heroic
 	if vars.raidMode == 2 or vars.raidMode == 3 then
 	
 		vars.unboundPlague = 20
@@ -73,6 +76,7 @@ function ProfessorPutricide_onEnterCombat( unit, event, attacker )
 	
 	unit:SendChatMessage( 14, 0, "Good news, everyone! I think I've perfected a plague that will destroy all life on Azeroth!" )
 	
+	-- spell: Ooze Tank Protection
 	unit:CastSpell( 71770 )
 
 end
@@ -148,20 +152,29 @@ function ProfessorPutricide_onDamageTaken( unit, event, damager, ammount )
 	
 	if vars.phase == 4 then
 	
-		if unit:GetHealthPct() < 80 then
-		
-			-- action change phase
-			
+		if unit:GetHealthPct() > 80 then
+			return
 		end
+		
+		-- react passive
+		unit:DisableCombat()
+			
+		-- change phase
+		ProfessorPutricide_doAction( unit, 6 )
+			
 	
 	elseif vars.phase == 5 then
 	
-		if unit:GetHealthPct() < 35 then
-		
-			-- action change phase
-		
+		if unit:GetHealthPct() > 35 then
+			return
 		end
-	
+		
+		-- react passive
+		unit:DisableCombat()
+			
+		-- change phase
+		ProfessorPutricide_doAction( unit, 6 )
+		
 	end
 
 end
@@ -170,26 +183,57 @@ function ProfessorPutricide_onReachWaypoint( unit, event, waypointId, foward )
 
 	print("ON REACH WP")
 	
+	local vars = self[ tostring( unit ) ]
+	
 	--
 	-- Festergut
 	--
 	if waypointId == 1 then
 	
-		-- do
+		-- Gas
+		ProfessorPutricide_doAction( unit, 1 )
 		
 	--
 	-- Rotface
 	--
 	elseif waypointId == 2 then
 	
-		-- do
+		-- Ooze
+		ProfessorPutricide_doAction( unit, 4 )
+		vars.rotfaceOozeFlood = 25
 	
 	--
-	-- Table
+	-- Table of Experiments
 	--
 	elseif waypointId == 3 then
 	
 		--unit:MoveToWaypoint()
+		
+		local go_table = unit:GetGameObjectNearestCoords( unit:GetX(), unit:GetY(), unit:GetZ(), 201584 )
+		
+		if go_table then 
+		
+			unit:SetFacing( go_table )
+		
+		end
+		
+		--local vars = self[ tostring( unit ) ]
+		
+		if vars.phase == 4 then
+		
+			-- spell: Create Concoction
+			unit:CastSpell( 71621 )
+			
+			vars.phaseTransition = 5 --todo
+		
+		elseif vars.phase == 5 then
+		
+			-- spell: Guzzle Potions
+			unit:CastSpell( 71893 )
+			
+			vars.phaseTransition = 5 --todo
+		
+		end
 	
 	end
 
@@ -223,11 +267,11 @@ function ProfessorPutricide_doAction( unit, action )
 		unit:CastSpell( 69125 )
 	
 	--
-	-- Festergut Death
+	-- Festergut death
 	--
 	elseif action == 2 then
-	
-		---todo
+		
+		vars.festergutDies = 4
 	
 	--
 	-- Rotface in combat
@@ -261,20 +305,76 @@ function ProfessorPutricide_doAction( unit, action )
 		end
 		
 		unit:Emote( 1 )
+		
+		-- dummy creature crap
+		
+		vars.oozeFloodStage = vars.oozeFloodStage + 1
+		
+		if vars.oozeFloodStage == 4 then
+
+			vars.oozeFloodStage = 0
+			
+		end
 	
 	--
-	-- Rotface Death
+	-- Rotface death
 	--
 	elseif action == 5 then
 	
-		--todo
+		vars.rotfaceDies = 5
 		
 	--
 	-- change phase
 	--
 	elseif action == 6 then
 	
-		--todo
+		-- not heroic
+		if not vars.raidMode == 2 or vars.raidMode == 3 then
+		
+			-- cast: Tear Gas
+			unit:CastSpell( 71617 )
+			
+			vars.tearGas = 3
+		
+		else
+		
+			unit:PlaySoundToSet( 17122 )
+			
+			unit:SendChatMessage( 14, 0, "Two oozes, one room. So many delightful possibilities!" )
+			
+			-- cast: Unstable Experiment
+			unit:CastSpell( 70351 )
+			
+			-- its 25 man
+			if vars.raidMode == 1 or vars.raidMode == 3 then
+			
+				-- lot of spell crap variables
+				-- lot of spell crap variables
+				-- lot of spell crap variables
+			
+			end
+			
+			-- move to table
+		
+		end
+		
+		if vars.phase == 3 then
+		
+			vars.phase = 4
+			
+			vars.malleableGoo = math.random( 21, 26 )
+			
+			vars.chockingGasBomb = math.random( 35, 40 )
+		
+		elseif vars.phase == 4 then
+		
+			vars.phase = 5
+			
+			vars.mutatedPlague = 25
+			
+			vars.unstableExperiment = nil
+		
+		end 
 	
 	end
 
@@ -282,17 +382,12 @@ end
 
 function ProfessorPutricide_onAIUpdate( unit, event )
 
-	print("AI UPDATE")
-	
-	if unit:GetNextTarget() == nil then
-		unit:WipeThreatList()
-		return 
-	end
+	--print("AI UPDATE")
 
 	local vars = self[ tostring( unit ) ]
 	
-	-- dont bother with this func in rotface and festergut phase
-	if vars.phase == 1 or vars.phase == 2 then
+	-- dont bother with this func if not rotface or festergut phase and not target
+	if not ( vars.phase == 1 or vars.phase == 2 ) and not unit:GetNextTarget() then
 		return
 	end
 
@@ -301,11 +396,46 @@ function ProfessorPutricide_onAIUpdate( unit, event )
 		return
 	end
 	
+	vars.festergutDies = vars.festergutDies - 1
+	vars.rotfaceDies = vars.rotfaceDies - 1
 	vars.berserk = vars.berserk - 1
 	vars.slimePuddle = vars.slimePuddle - 1
 	vars.unstableExperiment = vars.unstableExperiment - 1
 	
-	if vars.berserk <= 0 then
+	if vars.festergutDies <= 0 then
+	
+		unit:PlaySoundToSet( 17124 )
+		
+		unit:SendChatMessage( 14, 0, "Oh Festergut, you were always my favorite -- next to Rotface.  The good news is that you left behind so much gas. I can practically taste it!" )
+	
+	elseif vars.festergutGoo <= 0 then
+	
+		--cast aoe spell
+		
+		if vars.raidMode == 1 or vars.raidMode == 3 then
+		
+			vars.festergutGoo = math.random( 10, 15 )
+			
+		else
+		
+			vars.festergutGoo = math.random( 30, 35 )
+		
+		end
+	
+	elseif vars.rotfaceDies <= 0 then
+	
+		unit:PlaySoundToSet( 17146 )
+		
+		unit:SendChatMessage( 14, 0, "Terrible news, everyone. Rotface is dead, but great news everyone! He left behind plenty of ooze for me to use! What? I'm a poet and I didn't know it... Astounding!" )
+	
+	elseif vars.rotfaceOozeFlood <= 0 then
+	
+		-- Rotface Ooze
+		ProfessorPutricide_doAction( unit, 4 )
+		
+		vars.rotfaceOozeFlood = 25
+	
+	elseif vars.berserk <= 0 then
 	
 		unit:CastSpell( 47008 )
 		
@@ -319,6 +449,9 @@ function ProfessorPutricide_onAIUpdate( unit, event )
 	
 		-- todo target crap
 		
+		-- spell: Slime Puddle Trigger
+		unit:CastSpellOnTarget( 70341, unit:GetRandomPlayer() )
+		
 		vars.slimePuddle = 35
 		
 	elseif vars.unstableExperiment <= 0 then
@@ -328,6 +461,70 @@ function ProfessorPutricide_onAIUpdate( unit, event )
 		unit:SendChatMessage( 41, 0, "%s begins to cast Unstable Experiment!" )
 		
 		vars.unstableExperiment = math.random( 30, 35 )
+	
+	elseif vars.tearGas <= 0 then
+	
+		-- spell: Tear Gas Perioric Trigger
+		unit:CastSpell( 73170 )
+		
+	elseif vars.resumeAttack <= 0 then
+	
+		unit:EnableCombat()
+		
+		-- spell: Tear Gas Cancel
+		unit:CastSpell( 71620 )
+		
+	elseif vars.malleableGoo <= 0 then
+	
+		unit:CastSpellOnTarget( 70852, unit:GetRandomPlayer() )
+		
+		vars.malleableGoo = math.random( 25, 30 )
+		
+	elseif vars.chockingGasBomb <= 0 then
+		
+		unit:SendChatMessage( 41, 0, "%s cast |cFFFF7F00Choking Gas Bomb!|r" )
+		
+		unit:CastSpell( 71255 )
+		
+		vars.chockingGasBomb = math.random( 35, 40 )
+		
+	elseif vars.unboundPlague <= 0 then
+	
+		local target = unit:GetRandomPlayer()
+		
+		-- spell: Unbound Plague
+		unit:CastSpellOnTarget( 70911, target )
+		
+		-- spell: Unbound Plague Searcher
+		unit:CastSpellOnTarget( 70917, target )
+		
+		vars.unboundPlague = 90
+		
+	elseif vars.mutatedPlague <= 0 then
+	
+		unit:CastSpellOnTarget( 72451, unit:GetRandomPlayer() )
+		
+		vars.mutatedPlague = 10
+		
+	elseif vars.phaseTransition <= 0 then
+	
+		if vars.phase == 4 then
+		
+			unit:PlaySoundToSet( 17120 )
+			
+			unit:SendChatMessage( 14, 0, "Hrm, I don't feel a thing. Wha?! Where'd those come from?" )
+			
+			vars.resumeAttack = 6
+			
+		elseif vars.phase == 5 then
+		
+			unit:PlaySoundToSet( 17121 )
+			
+			unit:SendChatMessage( 14, 0, "Tastes like... Cherry! OH! Excuse me!" )
+			
+			vars.resumeAttack = 9
+			
+		end
 	
 	end
 	
